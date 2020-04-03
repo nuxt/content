f27aebf02f34e6b9782b449bcab2472e32199733
Benjamin Canac <canacb1@gmail.com>
2020-04-03 17:50:53 +0200

    chore(test): revert port setup

--- test/module.test.js
+++ test/module.test.js
@@ -1,19 +1,10 @@
 const { setup, loadConfig, get } = require('@nuxtjs/module-test-utils')
-const getPort = require('get-port')
 
 describe('module', () => {
   let nuxt
 
   beforeAll(async () => {
-    const port = await getPort()
-    const config = {
-      server: {
-        port
-      },
-      ...loadConfig(__dirname, '../../example')
-    };
-
-    ({ nuxt } = (await setup(config, { port })))
+    ({ nuxt } = (await setup(loadConfig(__dirname, '../../example'))))
   }, 60000)
 
   afterAll(async () => {
