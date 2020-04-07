02c09070672d317630d5aa735b5a01045311dd12
Benjamin Canac <canacb1@gmail.com>
2020-04-07 17:33:22 +0200

    chore(eslint): add rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   rules: {
     'vue/singleline-html-element-content-newline': 0,
     'vue/multiline-html-element-content-newline': 0,
-    'vue/html-self-closing': 0
+    'vue/html-self-closing': 0,
+    'vue/no-v-html': 0
   }
 }
