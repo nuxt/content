6a52131342d2288a6677074bb8343d5b4c816de8
Sébastien Chopin <seb@nuxtjs.com>
2020-04-15 18:12:29 +0200

    chore(starter): add link to gridsome starter

--- starters/blog/layouts/default.vue
+++ starters/blog/layouts/default.vue
@@ -3,14 +3,21 @@
     <main>
       <nuxt />
     </main>
-
-    <footer class="flex items-center justify-center py-8 px-4 text-gray-600 dark:text-gray-200">
-      Copyright © {{ new Date().getFullYear() }}. Powered by&nbsp;
+    <footer class="flex items-center justify-center pt-8 px-4 text-gray-700 dark:text-gray-200">
+      Copyright © {{ new Date().getFullYear() }} · Powered by&nbsp;
       <a
         href="https://nuxtjs.org"
         target="_blank"
         class="underline hover:text-gray-700 dark-hover:text-gray-400"
-      >Nuxt.js</a>
+      >Nuxt.js</a>.
+    </footer>
+    <footer class="flex items-center justify-center pt-2 pb-8 px-4 text-gray-600 dark:text-gray-200">
+      Starter inspired by&nbsp;
+      <a
+        href="https://github.com/gridsome/gridsome-starter-blog"
+        target="_blank"
+        class="underline hover:text-gray-700 dark-hover:text-gray-400"
+      >gridsome-starter-blog</a>.
     </footer>
   </div>
 </template>
@@ -34,6 +41,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-</style>
