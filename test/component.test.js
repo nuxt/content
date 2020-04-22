7d7a8774068a7e8fa5dcd3c311339f1ad275c1dd
Benjamin Canac <canacb1@gmail.com>
2020-04-22 17:54:56 +0200

    test: fix component timeout

--- test/component.test.js
+++ test/component.test.js
@@ -2,11 +2,12 @@ const { createBrowser } = require('tib')
 const { setup, loadConfig, url } = require('@nuxtjs/module-test-utils')
 
 describe('component', () => {
-  let nuxt, browser
+  let nuxt, browser, page
 
   beforeAll(async () => {
     ({ nuxt } = (await setup(loadConfig(__dirname))))
     browser = await createBrowser('puppeteer')
+    page = await browser.page(url('/'))
   }, 60000)
 
   afterAll(async () => {
@@ -15,7 +16,6 @@ describe('component', () => {
   })
 
   test('nuxt-content has generated html', async () => {
-    const page = await browser.page(url('/'))
     const html = await page.getHtml()
 
     expect(html).toContain('<h1>Home</h1> <div class="nuxt-content"><p>This is the home page!</p></div>')
