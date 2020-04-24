68a131db824fedbf4bf0adeb2366133e084bc8cf
Benjamin Canac <canacb1@gmail.com>
2020-04-24 18:35:03 +0200

    chore(starters): trigger deploy

--- starters/docs/plugins/components.js
+++ starters/docs/plugins/components.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+
 import IconCheck from '@/components/icons/Check.vue'
 import BaseList from '@/components/BaseList.vue'
 import CodeSandbox from '@/components/CodeSandbox.vue'
