be05df8073a23444452d72de21ea2db61cfeb8bf
Sébastien Chopin <seb@nuxtjs.com>
2020-04-25 15:14:48 +0200

    chore(starters): auto import base and icon components

--- starters/docs/plugins/components.js
+++ starters/docs/plugins/components.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 
-import IconCheck from '@/components/icons/Check.vue'
-import BaseList from '@/components/BaseList.vue'
-import CodeSandbox from '@/components/CodeSandbox.vue'
+/*
+** Import all components from `base/` and `icon/` directories
+** Learn more on require.context() on https://webpack.js.org/guides/dependency-management/#requirecontext
+*/
+function importComponents (prefix, r) {
+  r.keys().forEach((filename) => {
+    let Component = r(filename)
 
-Vue.component('IconCheck', IconCheck)
-Vue.component('BaseList', BaseList)
-Vue.component('CodeSandbox', CodeSandbox)
+    Component = Component.default || Component
+    Component.name = Component.name || (prefix + filename.replace(/^.+\//, '').replace(/\.\w+$/, ''))
+    Vue.component(Component.name, Component)
+  })
+}
+
+importComponents('Base', require.context('@/components/base', true, /\.(vue|js)$/))
+importComponents('Icon', require.context('@/components/icon', true, /\.(vue|js)$/))
