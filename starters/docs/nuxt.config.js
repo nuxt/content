5dccac37eda4879ed35a250c9e3ad8746114c48e
Benjamin Canac <canacb1@gmail.com>
2020-04-30 11:10:41 +0200

    chore(docs): improve metas

--- starters/docs/nuxt.config.js
+++ starters/docs/nuxt.config.js
@@ -8,7 +8,9 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'og:title', name: 'og:title', content: 'Nuxt Content' },
+      { hid: 'og:description', name: 'og:description', content: process.env.npm_package_description || '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
