5939d126bcffe00ee652a38bff6e83573b92a3ad
Sébastien Chopin <seb@nuxtjs.com>
2020-05-08 13:35:19 +0200

    docs: remove sass

--- docs/pages/_slug.vue
+++ docs/pages/_slug.vue
@@ -79,112 +79,94 @@ export default {
 }
 </script>
 
-<style lang="scss">
-.dark-mode {
-  .nuxt-content {
-    h2,
-    h3,
-    blockquote {
-      @apply border-gray-800;
-    }
+<style lang="postcss">
+.dark-mode .nuxt-content {
+  & h2,
+  & h3,
+  & blockquote {
+    @apply border-gray-800;
+  }
 
-    > code,
-    li > code,
-    p > code,
-    h3 > code {
-      @apply bg-gray-800;
+  & > code,
+  & li > code,
+  & p > code,
+  & h3 > code {
+    @apply bg-gray-800;
+  }
+}
+
+.nuxt-content h2 {
+  @apply text-3xl font-black mb-4 pb-1 border-b -mt-16 pt-24;
+
+  & > a {
+    @apply ml-6;
+    &::before {
+      content: '#';
+      @apply text-green-500 font-normal -ml-6 pr-1 absolute opacity-100;
+    }
+  }
+
+  &:hover {
+    & > a::before {
+      @apply opacity-100;
+    }
+  }
+}
+.nuxt-content h3 {
+  @apply text-2xl font-extrabold mb-2 pb-1 border-b -mt-16 pt-20;
+
+  & > a {
+    @apply ml-6;
+    &::before {
+      content: "#";
+      @apply text-green-500 font-normal -ml-5 pr-1 absolute opacity-100;
+    }
+  }
+
+  &:hover {
+    & > a::before {
+      @apply opacity-100;
     }
   }
 }
 
+@screen lg {
+  .nuxt-content h2 a,
+  .nuxt-content h3 a {
+    @apply ml-0;
+    &::before {
+      @apply opacity-0;
+    }
+  }
+}
+
+.nuxt-content ul,
+.nuxt-content ol {
+  @apply list-disc list-inside mb-4;
+
+  & > li {
+    @apply leading-7;
+
+    & > ul {
+      @apply pl-4;
+    }
+  }
+}
+
+.nuxt-content ol {
+  @apply list-decimal;
+}
+
 .nuxt-content {
-  h2 {
-    @apply text-3xl font-black mb-4 pb-1 border-b -mt-16 pt-24;
-
-    > a {
-      &::before {
-        content: "#";
-        @apply text-green-500 font-normal -ml-6 pr-1 absolute opacity-0;
-      }
-    }
-
-    &:hover {
-      > a::before {
-        @apply opacity-100;
-      }
-    }
-
-    @media (max-width: 640px) {
-      @apply ml-6;
-
-      > a {
-        &::before {
-          @apply opacity-100;
-        }
-      }
-    }
-  }
-
-  h3 {
-    @apply text-2xl font-extrabold mb-2 pb-1 border-b -mt-16 pt-20;
-
-    > a {
-      &::before {
-        content: "#";
-        @apply text-green-500 font-normal -ml-5 pr-1 absolute opacity-0;
-      }
-    }
-
-    &:hover {
-      > a::before {
-        @apply opacity-100;
-      }
-    }
-
-    @media (max-width: 640px) {
-      @apply ml-6;
-
-      > a {
-        &::before {
-          @apply opacity-100;
-        }
-      }
-    }
-  }
-
-  ul {
-    @apply list-disc list-inside mb-4;
-
-    > li {
-      @apply leading-7;
-
-      > ul {
-        @apply pl-4;
-      }
-    }
-  }
-
-  ol {
-    @apply list-decimal list-inside mb-4;
-
-    > li {
-      @apply leading-7;
-
-      > ol {
-        @apply pl-4;
-      }
-    }
-  }
-
-  a {
+  & a {
     @apply underline;
   }
 
-  p {
+  & p {
     @apply mb-4 leading-7;
   }
 
-  blockquote {
+  & blockquote {
     @apply py-2 pl-4 mb-4 border-l-4;
 
     > p:last-child {
@@ -192,17 +174,17 @@ export default {
     }
   }
 
-  > code,
-  li > code,
-  p > code {
+  & > code,
+  & li > code,
+  & p > code {
     @apply bg-gray-100 p-1 text-sm shadow-xs rounded;
   }
 
-  h3 > code {
+  & h3 > code {
     @apply bg-gray-100 p-1 text-lg shadow-xs rounded;
   }
 
-  pre[class*="language-"] {
+  & pre[class*="language-"] {
     @apply rounded mt-0 mb-4 bg-gray-800 text-sm relative;
 
     > code {
@@ -211,16 +193,16 @@ export default {
     }
   }
 
-  video {
+  & video {
     @apply w-full border rounded shadow-md;
   }
+}
 
-  &-highlight {
-    @apply relative;
+.nuxt-content-highlight {
+  @apply relative;
 
-    > .filename {
-      @apply absolute right-0 text-gray-600 font-light z-10 mr-2 mt-1 text-sm;
-    }
+  & > .filename {
+    @apply absolute right-0 text-gray-600 font-light z-10 mr-2 mt-1 text-sm;
   }
 }
 </style>
