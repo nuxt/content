7ade7b18b30b30ca4d47c48250f8d1e6464710e8
Salvador de la Puente González <salva@unoyunodiez.com>
2020-06-14 12:23:01 +0200

    test: vue.js component with content distribution

---
+++ test/fixture/content/vue-component.md
@@ -0,0 +1,5 @@
+<app-layout>
+  <template #header>Header content</template>
+  Main content
+  <template #footer>Footer content</template>
+</app-layout>
---
+++ test/fixture/pages/_slug.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ page.title }}</h1>
+    <nuxt-content :document="page" />
+  </div>
+</template>
+
+<script>
+import AppLayout from '../components/AppLayout'
+
+export default {
+  components: {
+    AppLayout // eslint-disable-line vue/no-unused-components
+  },
+  async asyncData ({ $content, params }) {
+    const page = await $content(params.slug || 'home').fetch()
+
+    return {
+      page
+    }
+  }
+}
+</script>
