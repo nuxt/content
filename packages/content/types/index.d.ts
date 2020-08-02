5d59d04811ab0c43d3a57ed0a46b99457ef3f3ba
Sebastian Krüger <2pi_r2@gmx.de>
2020-08-02 11:15:54 +0200

    add types for nuxt config in @nuxt/content

--- packages/content/types/index.d.ts
+++ packages/content/types/index.d.ts
@@ -1,4 +1,9 @@
+import '@nuxt/types'
 import './vuex'
+
+import { DumpOptions as yamlOptions } from "js-yaml";
+import { OptionsV2 as xmlOptions } from 'xml2js'
+import { CSVParseParam as csvOptions } from "node-csvtojson/v2/Parameters";
 
 interface NuxtContentInstance {
   only(keys: String | String[]): NuxtContentInstance
@@ -12,25 +17,53 @@ interface NuxtContentInstance {
   fetch<T = Result | Result[]>(): Promise<T>
 }
 
+type contentFunc = (...args: Array<String | Object>) => NuxtContentInstance
+
 type Result = (Object[] & {
   0: ('parallel' | 'sequential');
 });
 
 declare module '@nuxt/vue-app' {
   interface Context {
-    $content(...args: Array<String | Object>): NuxtContentInstance
+    $content: contentFunc
   }
 }
+
+type extendOrOverwrite<T> = ((old: T) => T) | T
 
 // Nuxt 2.9+
 declare module '@nuxt/types' {
   interface Context {
-    $content(...args: Array<String | Object>): NuxtContentInstance
+    $content: contentFunc
+  }
+
+  interface Configuration {
+    content: {
+      watch: boolean,
+      liveEdit: boolean,
+      apiPrefix: string,
+      dir: string,
+      fullTextSearchFields: extendOrOverwrite<Array<string>>,
+      nestedProperties: extendOrOverwrite<Array<string>>,
+      markdown: {
+        remarkPlugins: extendOrOverwrite<Array<string>>,
+        rehypePlugins: extendOrOverwrite<Array<string>>,
+        prism: {
+          theme: string | false
+        }
+      },
+      yaml: yamlOptions,
+      csv: csvOptions,
+      xml: xmlOptions,
+      extendParser: {
+        [extension: string]: (file: string) => any
+      }
+    }
   }
 }
 
 declare module 'vue/types/vue' {
   interface Vue {
-    $content(...args: Array<String | Object>): NuxtContentInstance
+    $content: contentFunc
   }
 }
