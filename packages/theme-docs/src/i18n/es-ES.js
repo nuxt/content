4103f500b566703e155e84d12d29bf11037f4a3e
Cristian Enguídanos Nebot <53697395+cenguidanos@users.noreply.github.com>
2020-08-05 10:27:50 +0200

    feat(theme-docs): add locale spanish support (#348)
    * add locale i18n es_ES.js
    
    * Add Locale es-ES.js i18n File

---
+++ packages/theme-docs/src/i18n/es-ES.js
@@ -0,0 +1,11 @@
+export default {
+  search: {
+    placeholder: 'Buscar en documentos (Pulsa "/" para afinar)'
+  },
+  toc: {
+    title: 'En esta página'
+  },
+  article: {
+    github: 'Editar esta página en Github'
+  }
+}
