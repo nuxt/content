8d9267279b5cf8ab78b4dd5b511bc22d972d9560
Benjamin Canac <canacb1@gmail.com>
2020-08-05 13:00:28 +0200

    feat(theme-docs): improve alert component

---
+++ packages/theme-docs/src/components/global/icons/IconCheckCircle.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>
+</template>
