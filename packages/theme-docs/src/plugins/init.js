e61f7f0992580215a0dc7cf93f49d2fb72f5f728
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2020-08-07 09:32:42 -0300

    fix: fetch releases before

--- packages/theme-docs/src/plugins/init.js
+++ packages/theme-docs/src/plugins/init.js
@@ -9,11 +9,11 @@ export default async function ({ store, app }) {
   if (process.client && !store.state.settings) {
     await store.dispatch('fetchSettings')
   }
-  if (process.client && !store.state.categories[app.i18n.locale]) {
-    await store.dispatch('fetchCategories')
-  }
   if (process.client && !store.state.releases.length) {
     await store.dispatch('fetchReleases')
+  }
+  if (process.client && !store.state.categories[app.i18n.locale]) {
+    await store.dispatch('fetchCategories')
   }
   if (process.client && !store.state.settings.defaultBranch) {
     await store.dispatch('fetchDefaultBranch')
