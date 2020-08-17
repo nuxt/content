71a7e29dff3db169e3e824c65bfbe8d8131d5df8
Benjamin Canac <canacb1@gmail.com>
2020-08-17 18:04:35 +0200

    feat(theme-docs): add badge component

---
+++ packages/theme-docs/src/components/global/base/Badge.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="inline-flex items-center px-3 py-1 leading-5 rounded-full text-base tracking-tight bg-primary-100 dark:bg-primary-900 text-primary-500"
+  >
+    <slot />
+  </div>
+</template>
