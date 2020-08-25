ff2f62e6b2d2b7864c79ec8f563f988eea91b7cb
Benjamin Canac <canacb1@gmail.com>
2020-08-25 11:23:47 +0200

    fix(theme-docs): minor graphic updates

--- packages/theme-docs/src/components/global/app/AppFooter.vue
+++ packages/theme-docs/src/components/global/app/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="h-16 relative w-full border-t dark:border-gray-800 bg-white dark:bg-gray-900 z-30">
+  <footer class="h-16 relative w-full border-t dark:border-gray-800 bg-white dark:bg-gray-900 z-20">
     <div class="h-full w-full container mx-auto px-4 lg:px-8 flex items-center justify-between">
       <div class="flex items-end">
         <a href="https://nuxtjs.org" target="_blank" rel="noopener">
