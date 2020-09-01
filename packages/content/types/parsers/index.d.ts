d8706329ea1bfa39d91fbb15e6e9df3712388627
Dennis <dennis.spierenburg@hotmail.com>
2020-09-01 14:14:20 +0200

    Removed index

--- packages/content/types/parsers/index.d.ts
+++
@@ -1,4 +0,0 @@
-export const CSV: typeof import("./csv");
-export const Markdown: typeof import("./markdown");
-export const XML: typeof import("./xml");
-export const YAML: typeof import("./yaml");
