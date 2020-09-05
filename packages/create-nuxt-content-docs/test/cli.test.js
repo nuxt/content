6008b67a93ac663da6e893e225136f7a1bf5106d
Nozomu Ikuta <nick.0508.nick@gmail.com>
2020-09-06 01:04:47 +0900

    chore: add a test case for create-nuxt-content

--- packages/create-nuxt-content-docs/test/cli.test.js
+++ packages/create-nuxt-content-docs/test/cli.test.js
@@ -3,21 +3,42 @@ const sao = require('sao')
 
 const generator = path.join(__dirname, '../src')
 
-test('should write files with good answers', async () => {
-  const answers = {
-    name: 'nuxt-content-docs',
-    title: 'Nuxt Content',
-    url: 'https://content.nuxtjs.org',
-    github: 'nuxt/content',
-    twitter: 'nuxt_js'
-  }
-  const stream = await sao.mock({ generator }, answers)
+describe('create-nuxt-content', () => {
+  test('should write files with good answers (GitHub)', async () => {
+    const answers = {
+      name: 'nuxt-content-docs',
+      title: 'Nuxt Content',
+      url: 'https://content.nuxtjs.org',
+      github: 'nuxt/content',
+      twitter: 'nuxt_js'
+    }
+    const stream = await sao.mock({ generator }, answers)
 
-  const pkg = await stream.readFile('package.json')
+    const pkg = await stream.readFile('package.json')
 
-  expect(JSON.parse(pkg)).toEqual(expect.objectContaining({
-    name: 'nuxt-content-docs',
-    version: '1.0.0',
-    private: true
-  }))
+    expect(JSON.parse(pkg)).toEqual(expect.objectContaining({
+      name: 'nuxt-content-docs',
+      version: '1.0.0',
+      private: true
+    }))
+  })
+
+  test('should write files with good answers (GitHub Enterprise)', async () => {
+    const answers = {
+      name: 'nuxt-content-docs',
+      title: 'Nuxt Content',
+      url: 'https://content.nuxtjs.org',
+      github: 'https://api.ghe.com/repos/me/project',
+      twitter: 'nuxt_js'
+    }
+    const stream = await sao.mock({ generator }, answers)
+
+    const pkg = await stream.readFile('package.json')
+
+    expect(JSON.parse(pkg)).toEqual(expect.objectContaining({
+      name: 'nuxt-content-docs',
+      version: '1.0.0',
+      private: true
+    }))
+  })
 })
