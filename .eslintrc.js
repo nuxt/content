c794739d97e594754ad59425aa0edb4cfaa396aa
Dennis <dennis.spierenburg@hotmail.com>
2020-09-09 10:39:03 +0200

    Set back editorconfig and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,6 @@ module.exports = {
     'vue/singleline-html-element-content-newline': 0,
     'vue/multiline-html-element-content-newline': 0,
     'vue/html-self-closing': 0,
-    'vue/no-v-html': 0,
-    'arrow-parens': 0,
-    semi: 0
+    'vue/no-v-html': 0
   }
-};
+}
