1111fecd892ac5c95eab80ae36fbbdbdd05bd225
sausin <sausin@users.noreply.github.com>
2020-09-19 14:56:12 +0530

    Update List.vue

--- packages/theme-docs/src/components/global/base/List.vue
+++ packages/theme-docs/src/components/global/base/List.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div v-for="(item, i) in items" :key="i" class="mt-3 flex">
-      <component :is="icon" class="h-6 w-6 mt-px mr-3 flex-shrink-0 text-primary-500" />
+      <span :class="`list-${type}`" class="mt-px mr-3 flex-shrink-0">
+        <component :is="icon" class="h-6 w-6" />
+      </span>
       {{ item }}
     </div>
   </div>
@@ -34,3 +36,23 @@ export default {
   }
 }
 </script>
+
+<style>
+
+/* Info */
+.list-info {
+  @apply text-blue-500;
+}
+/* Success */
+.list-success {
+  @apply text-green-500;
+}
+/* Warning */
+.list-warning {
+  @apply text-orange-500;
+}
+/* Danger */
+.list-danger {
+  @apply text-red-500;
+}
+</style>
