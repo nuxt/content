19ee400704f8d76cc7c5a09b2f9302794b8eb827
sausin <sausin@users.noreply.github.com>
2020-09-21 14:51:53 +0530

    feat(theme-docs): add variant and icon support in `list` component (#490)

--- packages/theme-docs/src/components/global/base/List.vue
+++ packages/theme-docs/src/components/global/base/List.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div v-for="(item, i) in items" :key="i" class="mt-3 flex">
-      <IconBadgeCheck class="h-6 w-6 mt-px mr-3 flex-shrink-0 text-primary-500" />
+      <span :class="`list-${type}`" class="mt-px mr-3 flex-shrink-0">
+        <component :is="icon" class="h-6 w-6" />
+      </span>
       {{ item }}
     </div>
   </div>
@@ -13,7 +15,44 @@ export default {
     items: {
       type: Array,
       default: () => []
+    },
+    type: {
+      type: String,
+      default: 'success',
+      validator (value) {
+        return ['info', 'success', 'warning', 'danger'].includes(value)
+      }
+    }
+  },
+  computed: {
+    icon () {
+      return ({
+        info: 'IconInformationCircle',
+        success: 'IconBadgeCheck',
+        warning: 'IconExclamationCircle',
+        danger: 'IconXCircle'
+      })[this.type]
     }
   }
 }
 </script>
+
+<style>
+
+/* Info */
+.list-info {
+  @apply text-blue-500;
+}
+/* Success */
+.list-success {
+  @apply text-green-500;
+}
+/* Warning */
+.list-warning {
+  @apply text-orange-500;
+}
+/* Danger */
+.list-danger {
+  @apply text-red-500;
+}
+</style>
