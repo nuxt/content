26ed4da0b500f7ab8486eed0036a4e2412c2b2c6
Benjamin Canac <canacb1@gmail.com>
2020-10-12 17:39:50 +0200

    fix(theme-docs): handle purgecss in rootDir components

--- packages/theme-docs/src/index.js
+++ packages/theme-docs/src/index.js
@@ -80,6 +80,10 @@ const defaultConfig = docsOptions => ({
       if (nuxt.options.dev) {
         nuxt.options.css.push(path.resolve(__dirname, 'assets/css/main.dev.css'))
       }
+      // Configure TailwindCSS
+      nuxt.hook('tailwindcss:config', function (defaultTailwindConfig) {
+        Object.assign(defaultTailwindConfig, defu(defaultTailwindConfig, tailwindConfig({ docsOptions, nuxt })))
+      })
     }
   },
   content: {
@@ -108,9 +112,7 @@ const defaultConfig = docsOptions => ({
       'DM+Mono': true
     }
   },
-  tailwindcss: {
-    config: tailwindConfig(docsOptions)
-  }
+  tailwindcss: {}
 })
 
 export default (userConfig) => {
