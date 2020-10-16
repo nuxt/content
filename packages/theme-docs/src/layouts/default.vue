d1b4ae0e16e5d737d2b1cc7803e8a703d5c46591
Sébastien Chopin <seb@nuxtjs.com>
2020-10-16 17:28:08 +0200

    fix(theme-docs): twitter preview when trailing slash on url

--- packages/theme-docs/src/layouts/default.vue
+++ packages/theme-docs/src/layouts/default.vue
@@ -28,6 +28,7 @@ export default {
   },
   head () {
     const i18nSeo = this.$nuxtI18nSeo()
+    const previewUrl = this.settings.url.replace(/\/$/, '') + '/preview.png'
 
     return {
       titleTemplate: (chunk) => {
@@ -46,12 +47,12 @@ export default {
         { hid: 'og:site_name', property: 'og:site_name', content: this.settings.title },
         { hid: 'og:type', property: 'og:type', content: 'website' },
         { hid: 'og:url', property: 'og:url', content: this.settings.url },
-        { hid: 'og:image', property: 'og:image', content: `${this.settings.url}/preview.png` },
+        { hid: 'og:image', property: 'og:image', content: previewUrl },
         // Twitter Card
         { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
         { hid: 'twitter:site', name: 'twitter:site', content: this.settings.twitter },
         { hid: 'twitter:title', name: 'twitter:title', content: this.settings.title },
-        { hid: 'twitter:image', name: 'twitter:image', content: `${this.settings.url}/preview.png` },
+        { hid: 'twitter:image', name: 'twitter:image', content: previewUrl },
         { hid: 'twitter:image:alt', name: 'twitter:image:alt', content: this.settings.title }
       ])
     }
