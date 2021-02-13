52e2ae7228e7b9414c6d9f799ed220e7221de8a9
Ndianabasi Udonkang <ndianabasi@furnish.ng>
2021-02-13 21:48:38 +0100

    chore: package updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint',
-    sourceType: 'module'
+    parser: "babel-eslint",
+    sourceType: "module"
   },
-  extends: [
-    '@nuxtjs'
-  ],
+  extends: ["@nuxtjs", "prettier"],
+  plugins: ["prettier"],
   rules: {
-    'vue/singleline-html-element-content-newline': 0,
-    'vue/multiline-html-element-content-newline': 0,
-    'vue/html-self-closing': 0,
-    'vue/no-v-html': 0
+    "vue/singleline-html-element-content-newline": 0,
+    "vue/multiline-html-element-content-newline": 0,
+    "vue/html-self-closing": 0,
+    "vue/no-v-html": 0
   }
-}
+};
