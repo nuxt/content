cc2bedfcc0dd27947d68df512ce8432901acdbcd
Luciano Tonet <tonetlds@gmail.com>
2021-07-01 17:13:13 -0300

    Add pt-BR support

---
+++ packages/theme-docs/src/i18n/pt-BR.js
@@ -0,0 +1,11 @@
+export default {
+  search: {
+    placeholder: 'Buscar em documentos (Pressione "/")'
+  },
+  toc: {
+    title: 'Nesta página'
+  },
+  article: {
+    github: 'Editar esta página no Github'
+  }
+}
