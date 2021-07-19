215c0f934d0dbf9ba2846c132bfa05d0a6b76841
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-07-19 16:25:57 +0200

    :bookmark: (1.0.17) release 1.0.17

--- src/i18n/languages/index.js
+++ src/i18n/languages/index.js
@@ -2,73 +2,73 @@ export default [
   {
     code: 'en',
     iso: 'en-US',
-    file: 'en-US.js',
+    file: 'en-US',
     name: 'English'
   },
   {
     code: 'de',
     iso: 'de-DE',
-    file: 'de-DE.js',
+    file: 'de-DE',
     name: 'German'
   },
   {
     code: 'es',
     iso: 'es-ES',
-    file: 'es-ES.js',
+    file: 'es-ES',
     name: 'Spanish'
   },
   {
     code: 'fr',
     iso: 'fr-FR',
-    file: 'fr-FR.js',
+    file: 'fr-FR',
     name: 'French'
   },
   {
     code: 'ja',
     iso: 'ja-JP',
-    file: 'ja-JP.js',
+    file: 'ja-JP',
     name: 'Japanese'
   },
   {
     code: 'ko',
     iso: 'ko-KR',
-    file: 'ko-KR.js',
+    file: 'ko-KR',
     name: 'Korean'
   },
   {
     code: 'ne',
     iso: 'ne-NP',
-    file: 'ne-NP.js',
+    file: 'ne-NP',
     name: 'Nepali'
   },
   {
     code: 'nl',
     iso: 'nl-BE',
-    file: 'nl-BE.js',
+    file: 'nl-BE',
     name: 'Dutch'
   },
   {
     code: 'pl',
     iso: 'pl-PL',
-    file: 'pl-PL.js',
+    file: 'pl-PL',
     name: 'Polish'
   },
   {
     code: 'ru',
     iso: 'ru-RU',
-    file: 'ru-RU.js',
+    file: 'ru-RU',
     name: 'Russian'
   },
   {
     code: 'tr',
     iso: 'tr-TR',
-    file: 'tr-TR.js',
+    file: 'tr-TR',
     name: 'Turkish'
   },
   {
     code: 'zh',
     iso: 'zh-CN',
-    file: 'zh-CN.js',
+    file: 'zh-CN',
     name: 'Chinese'
   }
 ]
