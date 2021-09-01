f16f8212483b222536aede65d095cfcd42cf99c8
Farnabaz <farnabaz@gmail.com>
2021-09-01 17:35:11 +0430

    fix: plugin injection

--- src/runtime/templates/context.js
+++ src/runtime/templates/context.js
@@ -8,10 +8,10 @@ const components = options.context.transformers.markdown.components.filter(item 
 import component_<%= item.name %> from '<%= item.path %>'
 <% }) %>
 <% remarkPlugins.forEach(item => { %>
-import remark_<%= item[0] %> from '<%= item[0] %>'
+import remark_<%= item[0] || item.name || item %> from '<%= item[0] || item.name || item %>'
 <% }) %>
 <% rehypePlugins.forEach(item => { %>
-  import rehype_<%= item[0] %> from '<%= item[0] %>'
+  import rehype_<%= item[0] || item.name || item %> from '<%= item[0] || item.name || item %>'
   <% }) %>
 
 const _context = <%= JSON.stringify(options.context) %>
@@ -20,23 +20,23 @@ _context.transformers.markdown.components = [
   <% components.forEach(item => { %>{
     name: '<%= item.name %>',
     instance: component_<%= item.name %>,
-    options: <%= JSON.stringify(item.options) %>
+    options: <%= JSON.stringify(item.options || {}) %>
   }, <% }) %>
 ]
 
 _context.transformers.markdown.remarkPlugins = [
   <% remarkPlugins.forEach(item => { %>{
-    name: '<%= item[0] %>',
-    instance: component_<%= item[0] %>,
-    options: <%= JSON.stringify(item[1]) %>
+    name: '<%= item[0] || item.name || item  %>',
+    instance: component_<%= item[0] || item.name || item %>,
+    options: <%= JSON.stringify(item[1] || item.options || {}) %>
   }, <% }) %>
 ]
 
 _context.transformers.markdown.rehypePlugins = [
   <% rehypePlugins.forEach(item => { %>{
-    name: '<%= item[0] %>',
-    instance: component_<%= item[0] %>,
-    options: <%= JSON.stringify(item[1]) %>
+    name: '<%= item[0] || item.name || item  %>',
+    instance: component_<%= item[0] || item.name || item %>,
+    options: <%= JSON.stringify(item[1] || item.options || {}) %>
   }, <% }) %>
 ]
 
