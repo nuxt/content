767427c066b122477aadcb5db120f2b5ba76a889
Farnabaz <farnabaz@gmail.com>
2021-12-08 11:15:06 +0330

    chore: use `cjs` extension

--- build.config.ts
+++ build.config.ts
@@ -4,7 +4,7 @@ export default defineBuildConfig({
   entries: [
     { input: 'src/index' },
     { input: 'src/runtime/', outDir: 'dist/runtime', format: 'esm' },
-    { input: 'src/runtime/', outDir: 'dist/runtime', format: 'cjs', declaration: false },
+    { input: 'src/runtime/', outDir: 'dist/runtime', ext: 'cjs', format: 'cjs', declaration: false },
     { input: 'src/templates/', outDir: 'dist/templates', declaration: false }
   ],
   declaration: true,
