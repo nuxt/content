f1b19505bfe79c9a96cfd966e097ca238365100e
hieubq <hieubq90@gmail.com>
2022-01-11 18:20:19 +0700

    Create vi-VN.js
    add i18n/vi-VN.js file for support Vietnamese

---
+++ packages/theme-docs/src/i18n/vi-VN.js
@@ -0,0 +1,12 @@
+export default {
+  search: {
+    placeholder: 'Tìm nội dung (Ấn "/" để tìm)'
+  },
+  toc: {
+    title: 'Nội dung gồm'
+  },
+  article: {
+    github: 'Chỉnh sửa nội dung',
+    updatedAt: 'Cập nhật lần cuối'
+  }
+}
