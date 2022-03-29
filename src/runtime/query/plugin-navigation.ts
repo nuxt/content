0326615f1e6b75d2327e8a68d2ef1a0a33196315
Yaël Guilloux <yael.guilloux@gmail.com>
2022-03-29 14:01:29 +0200

    fix(hotfix): fix plugin-navigation by hardcoding basepath (temporary)

--- src/runtime/query/plugin-navigation.ts
+++ src/runtime/query/plugin-navigation.ts
@@ -11,9 +11,25 @@ export interface Queries {
 export default defineQueryPlugin({
   name: 'navigation',
   queries: params => ({
-    findNavigation: () => $fetch(`/api/${useRuntimeConfig().content.basePath}/navigation`, {
-      method: 'POST',
-      body: params
-    })
+    findNavigation: () => {
+      /**
+       * TODO:
+       *
+       * Fix this basePath by reintroducing `useRuntimeConfig().content.basePath`
+       *
+       * The current usage of `useRuntimeConfig` in that context breaks when we used this code
+       * as an external (node_modules), and not in the local playground.
+       *
+       * As a workaround, I hardcoded the default basePath as no project currently rewrites it.
+       *
+       * We need to find a fix for this ASAP.
+       */
+      const basePath = '_content'
+
+      return $fetch(`/api/${basePath}/navigation`, {
+        method: 'POST',
+        body: params
+      })
+    }
   })
 })
