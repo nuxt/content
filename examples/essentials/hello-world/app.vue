938f34869437d445098bfc89ac4ef38e8687e1be
Clément Ollivier <clement.o2p@gmail.com>
2022-04-27 10:46:43 +0200

    docs(examples): hello world (#122)

---
+++ examples/essentials/hello-world/app.vue
@@ -0,0 +1,11 @@
+<script setup>
+const { data: document } = await useAsyncData('home', () => {
+  return queryContent().where({ slug: '/' }).findOne()
+})
+</script>
+
+<template>
+  <div>
+    <Content v-if="document" :document="document" />
+  </div>
+</template>
