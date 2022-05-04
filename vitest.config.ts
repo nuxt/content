aa0bb2bd7590443897ee1549057c2fd9e2e42f08
Farnabaz <farnabaz@gmail.com>
2022-05-04 12:19:21 +0200

    chore: clean `vitest.config`

--- vitest.config.ts
+++
@@ -1,7 +0,0 @@
-import { fileURLToPath } from 'url'
-import { defineConfig } from 'vite'
-import { resolve, dirname } from 'pathe'
-
-export const r = (...paths: string[]) => resolve(typeof __dirname === 'undefined' ? dirname(fileURLToPath(import.meta.url)) : __dirname, ...paths)
-
-export default defineConfig({})
