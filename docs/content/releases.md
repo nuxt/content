03411a82ffabb4131b1eaa6778c8792b36fbe557
Yaël Guilloux <yael.guilloux@gmail.com>
2022-05-06 05:47:53 +0200

    feat(docs): improve docs (upgrade docus ; add releases)

---
+++ docs/content/releases.md
@@ -0,0 +1,5 @@
+---
+toc: false
+---
+
+:releases
--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'development') {
 
 export default defineNuxtConfig({
   alias,
-  extends: ['./node_modules/docus/theme'],
+  extends: ['./node_modules/docus/packages/theme'],
   github: {
     repo: 'nuxt/content'
   },
