5e816083c125358d746e62e23b7f41b8e048802f
Sébastien Chopin <seb@nuxtjs.com>
2022-05-10 12:15:27 +0200

    chore: improve content components

--- src/runtime/components/Content.vue
+++ src/runtime/components/Content.vue
@@ -3,12 +3,19 @@ defineProps({
   document: {
     type: Object,
     required: true
+  },
+  excerpt: {
+    type: Boolean,
+    default: false
+  },
+  tag: {
+    type: String,
+    default: 'div'
   }
 })
 </script>
 
 <template>
-  <ContentRendererMarkdown v-if="document?.type === 'markdown'" :document="document" />
-  <ContentRendererYaml v-else-if="document?.type === 'yaml'" :document="document" />
+  <ContentRendererMarkdown v-if="document?.type === 'markdown'" :document="document" :excerpt="excerpt" :tag="tag" />
   <pre v-else>{{ document }}</pre>
 </template>
