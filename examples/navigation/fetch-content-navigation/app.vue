db12abf34290c62dc4640fd5ae202b2941aad3d7
Clément Ollivier <clement.o2p@gmail.com>
2022-05-10 11:47:05 +0200

    docs(navigation): create example

---
+++ examples/navigation/fetch-content-navigation/app.vue
@@ -0,0 +1,18 @@
+<script setup>
+const { data: document } = await useAsyncData('navigation', () => {
+  return fetchContentNavigation()
+})
+</script>
+
+<template>
+  <NuxtExampleLayout example="navigation/fetch-content-navigation" repo="nuxt/content">
+    <template #icon>
+      Nuxt/content
+    </template>
+    <main class="text-left">
+      <nav>
+        <AppNavigation :navigation-tree="document" />
+      </nav>
+    </main>
+  </NuxtExampleLayout>
+</template>
