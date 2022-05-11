f67f2bb381088f4c48647a76c50f9b298c450cd0
Ahad Birang <farnabaz@gmail.com>
2022-05-11 21:17:49 +0000

    fix: test

--- test/fixtures/basic/server/api/parse.ts
+++ test/fixtures/basic/server/api/parse.ts
@@ -1,10 +1,11 @@
 import { defineEventHandler, useBody } from 'h3'
+import { parseContent } from '#content/server'
 
 export default defineEventHandler(async (event) => {
   const { id, content } = await useBody(event)
 
   // @ts-ignore
-  const parsedContent = await contentParse(id, content).then(contentTransform)
+  const parsedContent = await parseContent(id, content)
 
   return parsedContent
 })
