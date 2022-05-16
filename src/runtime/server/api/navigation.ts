54c7b4a803882dc0df92a2d28b8c779b51f8ed60
Farnabaz <farnabaz@gmail.com>
2022-05-16 12:44:32 +0200

    fix: revert navigation changes

--- src/runtime/server/api/navigation.ts
+++ src/runtime/server/api/navigation.ts
@@ -15,7 +15,6 @@ export default defineEventHandler(async (event) => {
        */
       partial: false
     })
-    .sort({ path: 1, $numeric: true })
     .find()
 
   const dirConfigs = await serverQueryContent(event).where({ path: /\/_dir$/i, partial: true }).find()
