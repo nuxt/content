ce9ccb95da351143f05cbd5ebe573ecbe53c0101
Benjamin Canac <canacb1@gmail.com>
2022-05-16 14:55:57 +0200

    chore(server): remove admin driver that moved into admin module

--- src/runtime/server/preview.ts
+++ src/runtime/server/preview.ts
@@ -1,7 +1,5 @@
 import type { CompatibilityEvent } from 'h3'
 import { useQuery, useCookie } from 'h3'
-import { defineDriver } from 'unstorage'
-const publicConfig = useRuntimeConfig().public
 
 export const isPreview = (event: CompatibilityEvent) => {
   const previewToken = useQuery(event).previewToken || useCookie(event, 'previewToken')
@@ -13,54 +11,3 @@ export const getPreview = (event) => {
 
   return { key }
 }
-
-const adminDriver = defineDriver((options) => {
-  const { baseURL } = options
-
-  let memory = {}
-  let preview = null
-
-  return {
-    async getKeys (prefix: string) {
-      if (!prefix) {
-        return []
-      }
-
-      const [previewToken] = prefix.split(':')
-      const draft = await $fetch(`/api/projects/preview?token=${previewToken}`, { baseURL }).catch(_err => null)
-      if (!draft) {
-        return []
-      }
-      if (preview?.mtime !== draft.mtime) {
-        memory = {}
-        preview = draft
-        for (const addition of draft.additions) {
-          const { path, oldPath, content } = addition
-          const id = path.replace(/\//g, ':')
-
-          // Mark old path as deleted
-          if (oldPath) {
-            memory[`${previewToken}:${id}`] = { __deleted: true }
-          }
-
-          memory[`${previewToken}:${id}`] = content
-          memory[`${previewToken}:${id}$`] = { mtime: new Date(draft.mtime).toISOString() }
-        }
-        for (const deletion of draft.deletions) {
-          memory[`${previewToken}:${deletion.pathid}`] = { __deleted: true }
-        }
-      }
-      return Object.keys(memory).filter(key => !key.endsWith('$'))
-    },
-    getItem (key: string) {
-      return Promise.resolve(memory[key])
-    },
-    getMeta (key: string) {
-      return Promise.resolve(memory[`${key}$`])
-    }
-  }
-})
-// TODO: Move it to admin module
-useStorage().mount('content:source:preview', adminDriver({
-  baseURL: publicConfig.admin?.apiURL || 'https://dev-api.nuxt.com'
-}))
