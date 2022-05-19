c11f811974c58271dc868fa4d969fed61e52482f
Sébastien Chopin <seb@nuxtjs.com>
2022-05-19 14:15:39 +0200

    docs: update stackblitz icon

--- docs/components/content/IconStackBlitz.vue
+++ docs/components/content/IconStackBlitz.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg viewBox="0 0 28 28" aria-hidden="true">
-    <path d="M12.747 16.273h-7.46L18.925 1.5l-3.671 10.227h7.46L9.075 26.5l3.671-10.227z" fill="currentColor" />
+  <svg viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M11.46 14.773H4L17.638 0l-3.671 10.227h7.46L7.788 25l3.671-10.227h.001Z" fill="currentColor" />
   </svg>
 </template>
