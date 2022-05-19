c44a45f7fac3a08032525fdd7abd2288b6850e8a
Sébastien Chopin <seb@nuxtjs.com>
2022-05-19 03:48:21 +0200

    docs: improvements on button and icons

---
+++ docs/components/content/IconStackBlitz.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg viewBox="0 0 28 28" aria-hidden="true">
+    <path d="M12.747 16.273h-7.46L18.925 1.5l-3.671 10.227h7.46L9.075 26.5l3.671-10.227z" fill="currentColor" />
+  </svg>
+</template>
