fe6681fe3dfbc61f81589aa291fb4102f991f420
Sébastien Chopin <seb@nuxtjs.com>
2022-05-20 17:49:33 +0200

    fix(prose): remove default style

--- src/runtime/components/Prose/ProseCode.vue
+++ src/runtime/components/Prose/ProseCode.vue
@@ -20,35 +20,5 @@ defineProps({
 </script>
 
 <template>
-  <div>
-    <slot />
-  </div>
+  <slot />
 </template>
-
-<style scoped>
-div {
-  display: flex;
-  color: #d4d4d4;
-}
-
-:deep(pre) {
-  flex: 1 1 0%;
-  background-color: #2e3440;
-  padding: 1rem 0;
-}
-
-:deep(code) {
-  display: flex;
-  flex-direction: column;
-}
-
-:deep(.line) {
-  display: inline-table;
-  min-height: 15px;
-  padding: 0 1rem;
-}
-
-:deep(.line.highlight) {
-  background-color: #3f3f46;
-}
-</style>
