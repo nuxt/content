e84366c64bf11112b0ae52d8db05b713dcf87fdc
Yaël Guilloux <yael.guilloux@gmail.com>
2022-05-20 19:28:42 +0200

    feat(prose-code): fix prose-code

--- src/runtime/components/Prose/ProseCode.vue
+++ src/runtime/components/Prose/ProseCode.vue
@@ -1,24 +1,39 @@
-<script setup lang="ts">
-defineProps({
-  code: {
-    type: String,
-    default: ''
+<script lang="ts">
+import { defineComponent, useSlots, h } from '#imports'
+
+export default defineComponent({
+  props: {
+    code: {
+      type: String,
+      default: ''
+    },
+    language: {
+      type: String,
+      default: null
+    },
+    filename: {
+      type: String,
+      default: null
+    },
+    highlights: {
+      type: Array as () => number[],
+      default: () => []
+    }
   },
-  language: {
-    type: String,
-    default: null
-  },
-  filename: {
-    type: String,
-    default: null
-  },
-  highlights: {
-    type: Array as () => number[],
-    default: () => []
+  render (ctx) {
+    const { code, language, filename, highlights } = ctx
+    const slots = useSlots()
+
+    // Render the default slot if present
+    return slots?.default
+      ? slots.default({
+        code,
+        language,
+        filename,
+        highlights,
+        ...this.$attrs
+      })
+      : h('pre')
   }
 })
 </script>
-
-<template>
-  <slot />
-</template>
