f3e107c3d9ef11b9580d474745f8a76b47fe7813
Farnabaz <farnabaz@gmail.com>
2022-05-27 10:16:26 +0200

    fix: lint

--- src/runtime/server/transformers/index.ts
+++ src/runtime/server/transformers/index.ts
@@ -1,6 +1,7 @@
 import { extname } from 'pathe'
 import type { ParsedContent, ContentTransformer } from '../../types'
 import { getParser, getTransformers } from '#content/virtual/transformers'
+// eslint-disable-next-line import/named
 import { useNitroApp } from '#imports'
 /**
  * Parse content file using registered plugins
