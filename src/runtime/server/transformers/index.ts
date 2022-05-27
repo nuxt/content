20a66bde5250bc6f8e96620378fc2dad2ba78b45
Farnabaz <farnabaz@gmail.com>
2022-05-27 10:09:25 +0200

    feat: call hooks before and after parse

--- src/runtime/server/transformers/index.ts
+++ src/runtime/server/transformers/index.ts
@@ -6,6 +6,12 @@ import { getParser, getTransformers } from '#content/virtual/transformers'
  * Parse content file using registered plugins
  */
 export async function parseContent (id: string, content: string) {
+  const nitroApp = useNitroApp()
+
+  // Call hook before parsing the file
+  const file = { id, content }
+  await nitroApp.hooks.callHook('content:file:beforeParse', file)
+
   const ext = extname(id)
   const plugin: ContentTransformer = getParser(ext)
   if (!plugin) {
@@ -25,6 +31,9 @@ export async function parseContent (id: string, content: string) {
 
     return cur.transform!(next)
   }, Promise.resolve(parsed))
+
+  // Call hook after parsing the file
+  await nitroApp.hooks.callHook('content:file:afterParse', result)
 
   return result
 }
