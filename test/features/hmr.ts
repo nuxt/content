603ad9c401ed83bd8ceb60872401c7251305349f
Farnabaz <farnabaz@gmail.com>
2022-05-31 19:21:26 +0200

    fix: lint

--- test/features/hmr.ts
+++ test/features/hmr.ts
@@ -2,7 +2,7 @@ import fsp from 'fs/promises'
 import { test, expect } from 'vitest'
 import { join } from 'pathe'
 import { url } from '@nuxt/test-utils'
-import { renderPage, pullingForHMR, expectNoClientErrors, waitFor } from '../utils'
+import { renderPage, pullingForHMR, expectNoClientErrors } from '../utils'
 
 export const testHMR = async (fixturePath: string) => {
   await fsp.mkdir(join(fixturePath, 'content-tests'), { recursive: true })
