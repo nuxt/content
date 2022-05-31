71d5640be169ca65a248f7b679a2be42c6c703df
Ahad Birang <farnabaz@gmail.com>
2022-05-31 12:12:02 +0200

    feat: call hooks before and after parse (#1160)
    Co-authored-by: Sébastien Chopin <seb@nuxtjs.com>

--- src/runtime/server/transformers/index.ts
+++ src/runtime/server/transformers/index.ts
@@ -1,23 +1,27 @@
 import { extname } from 'pathe'
 import type { ParsedContent, ContentTransformer } from '../../types'
 import { getParser, getTransformers } from '#content/virtual/transformers'
-
+// eslint-disable-next-line import/named
+import { useNitroApp } from '#imports'
 /**
  * Parse content file using registered plugins
  */
 export async function parseContent (id: string, content: string) {
+  const nitroApp = useNitroApp()
+
+  // Call hook before parsing the file
+  const file = { _id: id, body: content }
+  await nitroApp.hooks.callHook('content:file:beforeParse', file)
+
   const ext = extname(id)
   const plugin: ContentTransformer = getParser(ext)
   if (!plugin) {
     // eslint-disable-next-line no-console
     console.warn(`${ext} files are not supported, "${id}" falling back to raw content`)
-    return {
-      _id: id,
-      body: content
-    }
+    return file
   }
 
-  const parsed: ParsedContent = await plugin.parse!(id, content)
+  const parsed: ParsedContent = await plugin.parse!(file._id, file.body)
 
   const transformers = getTransformers(ext)
   const result = await transformers.reduce(async (prev, cur) => {
@@ -25,6 +29,9 @@ export async function parseContent (id: string, content: string) {
 
     return cur.transform!(next)
   }, Promise.resolve(parsed))
+
+  // Call hook after parsing the file
+  await nitroApp.hooks.callHook('content:file:afterParse', result)
 
   return result
 }
