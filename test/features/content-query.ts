dd7a87193a5658f5a76158a6cc8938bab942b9ae
Farnabaz <farnabaz@gmail.com>
2022-06-07 12:23:11 +0200

    test: add prefix tests

--- test/features/content-query.ts
+++ test/features/content-query.ts
@@ -9,5 +9,35 @@ export const testContentQuery = () => {
       // Normal Prop
       expect(content).includes('Index')
     })
+
+    test('exact match foo not found', async () => {
+      const content = await $fetch('/features/query-content?path=/foo&findOne=1')
+
+      // empty
+      expect(content).includes('$$$$')
+    })
+
+    test('exact match foo/bar found', async () => {
+      const content = await $fetch('/features/query-content?path=/foo/bar&findOne=1')
+
+      // empty
+      expect(content).includes('prefix:foo:bar.md$$')
+    })
+
+    test('prefix queries', async () => {
+      const content = await $fetch('/features/query-content?path=/foo')
+
+      expect(content).includes('prefix:foo:bar.md')
+      expect(content).includes('prefix:foo:baz.md')
+      expect(content).includes('prefix:foobarbaz.md')
+    })
+
+    test('directory listing', async () => {
+      const content = await $fetch('/features/query-content?path=/foo/')
+
+      expect(content).includes('prefix:foo:bar.md')
+      expect(content).includes('prefix:foo:baz.md')
+      expect(content).not.includes('prefix:foobarbaz.md')
+    })
   })
 }
