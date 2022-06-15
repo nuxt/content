6fd21e6323d7af3594056de1dd010bab53b1f8ff
Yaël Guilloux <yael.guilloux@gmail.com>
2022-06-15 18:27:15 +0200

    docs(docs): improve navigation docs ; fix ReadMore component

---
+++ docs/components/content/ReadMore.vue
@@ -0,0 +1,28 @@
+<template>
+  <Alert>
+    <Icon class="inline-block w-5 h-5" name="heroicons-outline:information-circle" />
+    Read more in
+    <NuxtLink :to="link">
+      {{ computedTitle }}
+    </NuxtLink>.
+  </Alert>
+</template>
+
+<script setup lang="ts">
+import { splitByCase, upperFirst } from 'scule'
+
+const props = defineProps({
+  link: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: false
+  }
+})
+
+const createTitle = (title, link) => (title || link.split('/').filter(Boolean).map(part => splitByCase(part).map(p => upperFirst(p)).join(' ')).join(' > ').replace('Api', 'API'))
+
+const computedTitle = computed(() => createTitle(props.title, props.link))
+</script>
