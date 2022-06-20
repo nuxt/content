c9813923ee557a4acc37973a05e0e47f406b997b
Ahad Birang <farnabaz@gmail.com>
2022-06-20 18:16:10 +0200

    docs: fix build

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -12,28 +12,32 @@ if (process.env.NODE_ENV === 'development') {
 
 export default defineNuxtConfig({
   content: {
-    sources: {
-      v1: {
+    sources: [
+      {
+        name: 'v1',
         prefix: '/v1',
         driver: 'fs',
         base: resolve(__dirname, 'content-v1/en')
       },
-      'v1-ja': {
+      {
+        name: 'v1-ja',
         prefix: '/ja/v1',
         driver: 'fs',
         base: resolve(__dirname, 'content-v1/ja')
       },
-      'v1-fr': {
+      {
+        name: 'v1-fr',
         prefix: '/fr/v1',
         driver: 'fs',
         base: resolve(__dirname, 'content-v1/fr')
       },
-      'v1-ru': {
+      {
+        name: 'v1-ru',
         prefix: '/ru/v1',
         driver: 'fs',
         base: resolve(__dirname, 'content-v1/ru')
       }
-    },
+    ],
     highlight: {
       preload: ['xml']
     }
