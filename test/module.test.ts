c21f5f17c88d39da5afa1836b48c715e09c2e1ff
Ahad Birang <farnabaz@gmail.com>
2022-06-22 11:21:41 +0200

    feat(source): allow overwriting default source (#1273)
    * feat(source): allow overwriting default source
    
    * feat: sources object syntax
    
    * chore: update nuxt config
    
    * test: add test
    
    * docs: fix build
    
    * Apply suggestions from code review
    
    Co-authored-by: Sébastien Chopin <seb@nuxtjs.com>
    
    * fix: respect source name
    
    * chore: remove log
    
    Co-authored-by: Sébastien Chopin <seb@nuxtjs.com>

--- test/module.test.ts
+++ test/module.test.ts
@@ -4,8 +4,8 @@ import { useContentMounts } from '../src/utils'
 
 const nuxtDummy = { options: { rootDir: '/test', srcDir: '/test' } } as Nuxt
 
-describe('Content sources', () => {
-  test('Relative path', () => {
+describe('module', () => {
+  test('[sources] [array] Relative path', () => {
     const mounts = useContentMounts(nuxtDummy, ['content'])
     const mount = mounts['content:source:content']
 
@@ -14,7 +14,7 @@ describe('Content sources', () => {
     assert(mount.base === '/test/content')
   })
 
-  test('Absolute path', () => {
+  test('[sources] [array] Absolute path', () => {
     const mounts = useContentMounts(nuxtDummy, ['/content'])
     const mount = mounts['content:source:_content']
 
@@ -23,7 +23,7 @@ describe('Content sources', () => {
     assert(mount.base === '/content')
   })
 
-  test('Custom driver', () => {
+  test('[sources] [array] Custom driver', () => {
     const mounts = useContentMounts(nuxtDummy, [
       {
         name: 'repo1',
@@ -32,14 +32,12 @@ describe('Content sources', () => {
       }
     ])
 
-    assert(Object.keys(mounts).length === 1)
-
     expect(mounts).toMatchObject({
       'content:source:repo1': { driver: 'http', base: 'https://cdn.com' }
     })
   })
 
-  test('Multiple storages', () => {
+  test('[sources] [array] Multiple storages', () => {
     const mounts = useContentMounts(nuxtDummy, [
       'content',
       '/repo1/docs',
@@ -58,6 +56,47 @@ describe('Content sources', () => {
       'content:source:_repo1_docs': { driver: 'fs', base: '/repo1/docs' },
       'content:source:repo2_docs': { driver: 'fs', base: '/test/repo2/docs' },
       'content:source:repo1': { driver: 'http', base: 'https://cdn.com' }
+    })
+  })
+  test('[sources] [array] overwrite default source', () => {
+    const mounts = useContentMounts(nuxtDummy, [
+      {
+        name: 'content',
+        driver: 'http',
+        base: 'https://cdn.com'
+      }
+    ])
+
+    assert(Object.keys(mounts).length === 1)
+
+    expect(mounts).toMatchObject({
+      'content:source:content': { driver: 'http', base: 'https://cdn.com' }
+    })
+  })
+
+  test('[sources] [object] overwrite default source', () => {
+    const mounts = useContentMounts(nuxtDummy, {
+      content: {
+        driver: 'http',
+        base: 'https://cdn.com'
+      }
+    })
+
+    expect(mounts).toMatchObject({
+      'content:source:content': { driver: 'http', base: 'https://cdn.com' }
+    })
+  })
+
+  test('[sources] [object] secondary source', () => {
+    const mounts = useContentMounts(nuxtDummy, {
+      secondary: {
+        driver: 'http',
+        base: 'https://cdn.com'
+      }
+    })
+
+    expect(mounts).toMatchObject({
+      'content:source:secondary': { driver: 'http', base: 'https://cdn.com' }
     })
   })
 })
