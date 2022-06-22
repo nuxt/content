d6b9c0334fdd600404d6e760f337421317bc5bd6
Yaël Guilloux <yael.guilloux@gmail.com>
2022-06-22 17:43:12 +0200

    fix(tests): fix tests and improve `yarn test` command

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 CWD=$(pwd)
-ARG1=${1:-basic}
-FIXTURE_PATH=$CWD/test/fixtures/$ARG1
+ARG1=${1}
 
-find $CWD/test -name ".nuxt" -exec rm -rf {} \;
+# Remove all .nuxt directories in the test/fixtures directory
+for d in $(find $CWD/test/fixtures -maxdepth 1 -mindepth 1 -type d); do
+  cd $d
+  rm -rf .nuxt
+  npx nuxi prepare
+  cd $CWD
+done
 
-(nuxi prepare $FIXTURE_PATH && npx vitest run -t $ARG1)
+if [[ $ARG1 ]]
+then
+  (npx vitest run -t $ARG1)
+else
+  (npx vitest run)
+fi
