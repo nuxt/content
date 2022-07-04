7041f6ad224f5da6049bbb094ff24ffec4ac5e3b
Daniel Roe <daniel@roe.dev>
2022-07-04 17:48:37 +0100

    fix: freeze current page

--- src/runtime/pages/document-driven.vue
+++ src/runtime/pages/document-driven.vue
@@ -2,13 +2,14 @@
 import { useContent, useContentHead } from '#imports'
 
 const { page } = useContent()
+const currentPage = page.value
 
 useContentHead(page)
 </script>
 
 <template>
   <div class="document-driven-page">
-    <ContentRenderer v-if="page" :key="page._id" :value="page">
+    <ContentRenderer v-if="currentPage" :key="currentPage._id" :value="currentPage">
       <template #empty="{ value }">
         <DocumentDrivenEmpty :value="value" />
       </template>
