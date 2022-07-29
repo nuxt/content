f6b67716e7e9ede5aa7fcdf9efb65d504dc00215
Ahad Birang <farnabaz@gmail.com>
2022-07-29 16:06:59 +0430

    fix(document-driven): throw 404 error when content is missing

--- src/runtime/pages/document-driven.vue
+++ src/runtime/pages/document-driven.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
+import { throwError } from '#app'
+import { createError } from 'h3'
 import { useContent, useContentHead } from '#imports'
 
 const { page } = useContent()
+
+// Page not found
+if (!page.value) {
+  throwError(
+    createError({
+      statusCode: 404,
+      statusMessage: `Page not found: ${useRoute().path}`
+    })
+  )
+}
 
 useContentHead(page)
 </script>
