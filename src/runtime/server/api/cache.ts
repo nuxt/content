2bf5cb7ba068ecc8f94722b8c4603d20a01861c4
Ahad Birang <farnabaz@gmail.com>
2022-08-04 17:18:24 +0430

    feat: create navigation cache

--- src/runtime/server/api/cache.ts
+++ src/runtime/server/api/cache.ts
@@ -1,18 +1,18 @@
 import { defineEventHandler } from 'h3'
+import { getContentIndex } from '../content-index'
 import { cacheStorage, serverQueryContent } from '../storage'
 
 // This route is used to cache all the parsed content
 export default defineEventHandler(async (event) => {
   const now = Date.now()
   // Fetch all content
-  const data = await serverQueryContent(event).find()
+  await serverQueryContent(event).find()
 
   // Generate Index
-  const index = data.reduce((acc, item) => {
-    acc[item._path!] = item._id
-    return acc
-  }, {} as Record<string, string>)
-  await cacheStorage.setItem('index.json', index)
+  await getContentIndex(event)
+
+  const navigation = await $fetch('/api/_content/navigation')
+  await cacheStorage.setItem('content-navigation.json', navigation)
 
   return {
     generatedAt: now,
