2d2bfa32b8fc905ffc8b8c98d1af74cb5c9d39f7
Ahad Birang <farnabaz@gmail.com>
2022-09-06 15:21:59 +0200

    remove layout and duplicate components

--- docs/components/content/IconCodeSandBox.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <svg viewBox="0 0 105 104" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M104.297.31104.87354.3111V103.735H104.297V.31104ZM93.7196 93.1572V10.8885H11.4509v82.2687h82.2687Z" fill="currentColor" />
-  </svg>
-</template>
