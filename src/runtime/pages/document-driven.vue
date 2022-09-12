acd6dba41a437b3181af148e90cf1aa19b930969
Ahad Birang <farnabaz@gmail.com>
2022-09-12 12:48:52 +0200

    fix(document-driven): sync page layout

--- src/runtime/pages/document-driven.vue
+++ src/runtime/pages/document-driven.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useContent, useContentHead, useRequestEvent } from '#imports'
 
-const { page } = useContent()
+const { page, layout } = useContent()
 
 // Page not found, set correct status code on SSR
 if (!page.value && process.server) {
@@ -14,7 +14,7 @@ useContentHead(page)
 
 <template>
   <div class="document-driven-page">
-    <NuxtLayout :name="page?.layout || 'default'">
+    <NuxtLayout :name="layout || 'default'">
       <ContentRenderer v-if="page" :key="page._id" :value="page">
         <template #empty="{ value }">
           <DocumentDrivenEmpty :value="value" />
