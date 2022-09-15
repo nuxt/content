4414b91051b6043ac9bcbe8361849823d77da696
Ahad Birang <farnabaz@gmail.com>
2022-09-15 19:52:12 +0200

    fix(document-driven): disable static payload (#1526)

--- src/runtime/plugins/documentDriven.ts
+++ src/runtime/plugins/documentDriven.ts
@@ -261,6 +261,10 @@ export default defineNuxtPlugin((nuxt) => {
     if (redirect) { return redirect }
   })
 
+  if (process.server) {
+    delete nuxt.payload.prerenderedAt
+  }
+
   // @ts-ignore - Refresh on client-side
   nuxt.hook('app:data:refresh', async () => process.client && await refresh(useRoute(), true))
 })
