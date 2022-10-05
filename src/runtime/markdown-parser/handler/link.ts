cb7679e3acbad6dc2e51aa351f2350447d20eeec
Percy Ma <kecrily@gmail.com>
2022-10-05 17:17:59 +0800

    fix: use relative `.md` link (#1556)
    Co-authored-by: Ahad Birang <farnabaz@gmail.com>

--- src/runtime/markdown-parser/handler/link.ts
+++ src/runtime/markdown-parser/handler/link.ts
@@ -4,6 +4,7 @@ import type { H } from 'mdast-util-to-hast'
 import { all } from 'mdast-util-to-hast'
 import { encode } from 'mdurl'
 import type { MdastNode } from 'mdast-util-to-hast/lib'
+import { isRelative } from 'ufo'
 
 type Node = MdastNode & {
   title: string
@@ -16,7 +17,7 @@ type Node = MdastNode & {
 export default function link (h: H, node: Node) {
   const props: any = {
     ...((node.attributes || {}) as object),
-    href: encode(node.url)
+    href: encode(normalizeLink(node.url))
   }
 
   if (node.title !== null && node.title !== undefined) {
@@ -24,4 +25,15 @@ export default function link (h: H, node: Node) {
   }
 
   return h(node, 'a', props, all(h, node))
+}
+
+function normalizeLink (link: string) {
+  if (isRelative(link) || (!/^https?/.test(link) && !link.startsWith('/'))) {
+    return link.split('/')
+      .map(x => x.replace(/^[0-9]*\./g, ''))
+      .join('/')
+      .replace(/\.md$/g, '')
+  } else {
+    return link
+  }
 }
