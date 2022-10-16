005fdfd19aa9658f821cb64a868d7f4d09582e1f
nobkd <44443899+nobkd@users.noreply.github.com>
2022-10-16 23:06:35 +0200

    fix(anchorLinks): add useRuntimeConfig imports (#1605)

--- src/runtime/components/Prose/ProseH4.vue
+++ src/runtime/components/Prose/ProseH4.vue
@@ -8,6 +8,7 @@
 </template>
 
 <script setup lang="ts">
+import { useRuntimeConfig } from '#imports'
 defineProps<{ id: string }>()
 const heading = 4
 const { anchorLinks } = useRuntimeConfig().public.content
