3c683ce98e57795c54de892748e03357b2c5aa64
Farnabaz <farnabaz@gmail.com>
2023-01-24 02:24:21 +0100

    fix(markdown): images src with `baseURL`

--- src/runtime/components/Prose/ProseImg.vue
+++ src/runtime/components/Prose/ProseImg.vue
@@ -1,9 +1,12 @@
 <template>
-  <img :src="src" :alt="alt" :width="width" :height="height">
+  <img :src="refinedSrc" :alt="alt" :width="width" :height="height">
 </template>
 
 <script setup lang="ts">
-defineProps({
+import { withBase } from 'ufo'
+import { useRuntimeConfig, computed } from '#imports'
+
+const props = defineProps({
   src: {
     type: String,
     default: ''
@@ -20,5 +23,12 @@ defineProps({
     type: [String, Number],
     default: undefined
   }
+})
+
+const refinedSrc = computed(() => {
+  if (props.src?.startsWith('/') && !props.src.startsWith('//')) {
+    return withBase(props.src, useRuntimeConfig().app.baseURL)
+  }
+  return props.src
 })
 </script>
