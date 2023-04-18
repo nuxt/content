7c58d0f66bb25c077946850ed8376b1591e51c0e
Dave Stewart <dave@davestewart.co.uk>
2023-04-18 16:11:28 +0100

    fix: simplify ignores factory

--- src/runtime/utils/config.ts
+++ src/runtime/utils/config.ts
@@ -4,9 +4,7 @@
 export function makeIgnored (content: { ignores: string[], experimental: { ignores?: string[] } }): (key: string) => boolean {
   // 2.7+ supports free regexp + slashes
   if (content.experimental.ignores) {
-    const ignores = content.experimental.ignores
-    const defaults = ['/\\.', '/-']
-    const rxAll = [...defaults, ...ignores].map(p => new RegExp(p))
+    const rxAll = ['/\\.', '/-', ...content.experimental.ignores].map(p => new RegExp(p))
     return function isIgnored (key: string): boolean {
       const path = '/' + key.replaceAll(':', '/')
       return rxAll.some(rx => rx.test(path))
@@ -14,9 +12,7 @@ export function makeIgnored (content: { ignores: string[], experimental: { ignor
   }
 
   // 2.6 prefixed by unstorage delimiters
-  const ignores = content.ignores
-  const defaults = ['\\.', '-']
-  const rxAll = [...defaults, ...ignores].map(p => new RegExp(`^${p}|:${p}`))
+  const rxAll = ['\\.', '-', ...content.ignores].map(p => new RegExp(`^${p}|:${p}`))
   return function isIgnored (key: string): boolean {
     return rxAll.some(rx => rx.test(key))
   }
