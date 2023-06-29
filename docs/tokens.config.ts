412bc2e3ee5a041e45876997c161df133fe78bfd
Serhii Bedrytskyi <bedrytskyi@gmail.com>
2023-06-29 17:56:53 +0300

    fix: primary color tokens

--- docs/tokens.config.ts
+++ docs/tokens.config.ts
@@ -3,16 +3,16 @@ import { defineTheme } from 'pinceau'
 export default defineTheme({
   color: {
     primary: {
-      50: { value: '#ecfdf5' },
-      100: { value: '#d1fae5' },
-      200: { value: '#a7f3d0' },
-      300: { value: '#6ee7b7' },
-      400: { value: '#34d399' },
-      500: { value: '#10b981' },
-      600: { value: '#059669' },
-      700: { value: '#047857' },
-      800: { value: '#065f46' },
-      900: { value: '#064e3b' }
+      50: { value: '#F5FFFC' },
+      100: { value: '#DDF9EF' },
+      200: { value: '#BDF3E1' },
+      300: { value: '#64E4BA' },
+      400: { value: '#28CF98' },
+      500: { value: '#10B981' },
+      600: { value: '#098B60' },
+      700: { value: '#026443' },
+      800: { value: '#003826' },
+      900: { value: '#001F15' }
     }
   }
 })
