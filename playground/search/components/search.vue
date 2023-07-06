d34ff7014b3dc6a6082efbb5eead06de03a1ab08
Barbapapazes <e.soubiran25@gmail.com>
2023-07-06 11:37:40 +0200

    feat: pass options to `useFuse` from `useSearch`

--- playground/search/components/search.vue
+++ playground/search/components/search.vue
@@ -1,6 +1,20 @@
 <script lang="ts" setup>
 const search = ref('')
-const result = await useSearch(search)
+
+const options =
+  {
+    fuseOptions: {
+      keys: [
+        'title'
+      ],
+      ignoreLocation: true,
+      threshold: 0,
+      includeMatches: true
+    },
+    matchAllWhenSearchEmpty: true
+  }
+
+const result = await useSearch(search, options)
 </script>
 
 <template>
--- src/runtime/composables/search.ts
+++ src/runtime/composables/search.ts
@@ -1,7 +1,7 @@
-import { useFuse } from '@vueuse/integrations/useFuse'
+import { UseFuseOptions, useFuse } from '@vueuse/integrations/useFuse'
 import { useRuntimeConfig, useLazyFetch } from '#imports'
 
-export const useSearch = async (query: MaybeRefOrGetter<string>) => {
+export const useSearch = async <DataItem>(query: MaybeRefOrGetter<string>, options: MaybeRefOrGetter<UseFuseOptions<DataItem>>) => {
   const runtimeConfig = useRuntimeConfig()
   const integrity = runtimeConfig.public.content.integrity
   const baseAPI = runtimeConfig.public.content.api.baseURL
@@ -9,24 +9,7 @@ export const useSearch = async (query: MaybeRefOrGetter<string>) => {
   const { data } = await useLazyFetch(`${baseAPI}/search${integrity ? '.' + integrity : ''}.json`)
 
   // TODO: add a way to configure the search (using options from search?)
-  const { results } = useFuse(query,
-    data,
-    {
-      fuseOptions: {
-        keys: [
-          'title',
-          'description',
-          'keywords',
-          'body'
-        ],
-        ignoreLocation: true,
-        threshold: 0,
-        includeMatches: true,
-        includeScore: true
-      },
-      matchAllWhenSearchEmpty: true
-    }
-  )
+  const { results } = useFuse(query, data, options)
 
   return results
 }
