2c76e2d2a67011409d07f707127a6537e79382c0
Estéban <e.soubiran25@gmail.com>
2023-07-18 15:54:31 +0200

    fix: allow empty string in `$contains` (#2179)

---
+++ playground/basic/pages/custom-query.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+const { data } = await useAsyncData('posts', () => queryContent('/real-content/post').where({ authors: { $contains: '' } }).find())
+</script>
+
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
