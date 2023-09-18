5f059f81478ef6e64c5a63db9da24b80ae337176
Estéban <e.soubiran25@gmail.com>
2023-09-18 18:02:03 +0200

    feat: update types (#2156)

---
+++ playground/document-driven/types/article.ts
@@ -0,0 +1,9 @@
+import { ParsedContent } from '../../../src/runtime/types'
+
+export interface ParsedArticle extends ParsedContent {
+  cover: {
+    image: string,
+    alt: string
+  }
+  publishedAt: string
+}
