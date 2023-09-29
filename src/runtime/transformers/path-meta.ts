1257cb5c93d00e091e7b17d4449154af185834a3
Farnabaz <farnabaz@gmail.com>
2023-09-29 13:58:28 +0200

    fix(path-meta): prevent `undefined` error

--- src/runtime/transformers/path-meta.ts
+++ src/runtime/transformers/path-meta.ts
@@ -6,12 +6,16 @@ import { defineTransformer } from './utils'
 
 const SEMVER_REGEX = /^(\d+)(\.\d+)*(\.x)?$/
 
-const describeId = (_id: string) => {
-  const [_source, ...parts] = _id.split(':')
+export const describeId = (id: string) => {
+  const [_source, ...parts] = id.split(':')
 
-  const [, filename, _extension] = parts[parts.length - 1].match(/(.*)\.([^.]+)$/)!
-  parts[parts.length - 1] = filename
-  const _path = parts.join('/')
+  const [, filename, _extension] = parts[parts.length - 1]?.match(/(.*)\.([^.]+)$/) || []
+
+  if (filename) {
+    parts[parts.length - 1] = filename
+  }
+
+  const _path = (parts || []).join('/')
 
   return {
     _source,
