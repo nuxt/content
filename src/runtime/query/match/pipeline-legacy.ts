4b9b6b7ad813aed0170985f8aee361f98ea4080e
Farnabaz <farnabaz@gmail.com>
2024-01-30 16:45:49 +0100

    fix(client-db): fetch dir config on legacy query

--- src/runtime/query/match/pipeline-legacy.ts
+++ src/runtime/query/match/pipeline-legacy.ts
@@ -6,6 +6,10 @@ export function createPipelineFetcherLegacy<T> (getContentsList: () => Promise<T
   const _pipelineFetcher = createPipelineFetcher(getContentsList)
 
   return async (query: QueryBuilder<T>): Promise<T | T[]> => {
+    // fetch dir config to match api behavior
+    if (query.params().first) {
+      (query as unknown as ContentQueryBuilder<T>).withDirConfig()
+    }
     const params = query.params()
     const result = await _pipelineFetcher(query as unknown as ContentQueryBuilder<T>)
 
