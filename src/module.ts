17862e3c9be180bb4a376782d7865fcaf8f55070
Daniel Roe <daniel@roe.dev>
2024-03-10 17:04:35 -0700

    fix: optimise `slugify` with nested config

--- src/module.ts
+++ src/module.ts
@@ -408,7 +408,7 @@ export default defineNuxtModule<ModuleOptions>({
     extendViteConfig((config) => {
       config.optimizeDeps = config.optimizeDeps || {}
       config.optimizeDeps.include = config.optimizeDeps.include || []
-      config.optimizeDeps.include.push('slugify')
+      config.optimizeDeps.include.push('@nuxtjs/content > slugify')
 
       config.plugins?.push({
         name: 'content-slot',
