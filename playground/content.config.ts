6a27341faa65219166ce7c70ba81d6605e911594
Farnabaz <farnabaz@gmail.com>
2024-09-20 17:41:06 +0200

    chore: update playground

--- playground/content.config.ts
+++ playground/content.config.ts
@@ -1,46 +1,66 @@
 import { z } from 'zod'
-import { defineCollection, contentSchema } from '@farnabaz/content-next'
+import { defineCollection } from '@farnabaz/content-next'
 
-export const collections = [
-  defineCollection('content', {
-    source: {
-      driver: 'fs',
-      base: '~~/content',
-      ignore: [
-        '**/content/data/**',
-        '**/content/pages/**',
-      ],
-    },
-    schema: contentSchema,
-  }),
-  defineCollection('data', {
-    source: '~~/content/data',
-    schema: z.object({
-      path: z.string(),
-      company: z.string(),
-      domain: z.array(z.string()),
-      tutorial: z.array(
-        z.record(
-          z.object({
-            name: z.string(),
-            type: z.string(),
-            born: z.number(),
-          }),
-        ),
-      ),
-      author: z.string(),
-      published: z.boolean(),
-    }),
-  }),
-  defineCollection('pages', {
-    source: '~~/content/pages',
-    schema: z.object({
-      authors: z.array(
+const content = defineCollection({
+  type: 'page',
+  source: {
+    driver: 'fs',
+    base: '**',
+    ignore: [
+      'data/**',
+      'pages/**',
+      'nuxt-content/**',
+    ],
+  },
+})
+
+const data = defineCollection({
+  type: 'page',
+  source: 'data/**',
+  schema: z.object({
+    path: z.string(),
+    company: z.string(),
+    domain: z.array(z.string()),
+    tutorial: z.array(
+      z.record(
         z.object({
           name: z.string(),
-          email: z.string(),
+          type: z.string(),
+          born: z.number(),
         }),
       ),
+    ),
+    author: z.string(),
+    published: z.boolean(),
+  }),
+})
+
+const pages = defineCollection({
+  type: 'page',
+  source: 'pages/**',
+})
+
+const nuxt_content = defineCollection({
+  type: 'page',
+  source: {
+    driver: 'fs',
+    base: 'nuxt-content/**',
+    prefix: '/nuxt-content',
+  },
+})
+
+export const collections = {
+  content,
+  data,
+  pages,
+  nuxt_content,
+  foo: defineCollection({
+    source: 'data/foo/bar.yml',
+    type: 'data',
+    schema: z.object({
+      name: z.string(),
+      type: z.string(),
+      born: z.number(),
     }),
   }),
-]
+}
