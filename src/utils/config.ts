8f60928088506ea788436d57220b4925bec0abeb
Farnabaz <farnabaz@gmail.com>
2024-09-26 15:27:34 +0200

    fix: use `jiti@2` to import `content.config.ts`

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,9 +1,12 @@
 import { join } from 'node:path'
 import fastGlob from 'fast-glob'
+import { createJiti } from 'jiti'
+import type { DefinedCollection } from '../types'
 import { resolveCollections } from './collection'
 import { generateInitialFiles } from './dev'
 
 export async function loadContentConfig(root: string, opts: { createOnMissing?: boolean } = {}) {
+  const jiti = createJiti(root)
   const configs = await fastGlob('content.config.*', { cwd: root })
   let configPath = configs.length ? join(root, configs[0]) : undefined
 
@@ -11,13 +14,13 @@ export async function loadContentConfig(root: string, opts: { createOnMissing?: 
     configPath = await generateInitialFiles(root)
   }
 
-  const contentConfig = configPath
-    ? await import(configPath)
+  const contentConfig = (configPath
+    ? await jiti.import(configPath)
       .catch((err) => {
         console.error(err)
-        return []
+        return {}
       })
-    : {}
+    : {}) as { collections: Record<string, DefinedCollection> }
 
   return {
     collections: resolveCollections(contentConfig.collections || {}),
