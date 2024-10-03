4a80424ce821f94947646a70aea96235f1af9427
Farnabaz <farnabaz@gmail.com>
2024-10-03 14:17:07 +0200

    fix: convert `NULL` column to undefined

--- src/runtime/utils/internal/parseJsonFields.ts
+++ src/runtime/utils/internal/parseJsonFields.ts
@@ -5,5 +5,11 @@ export function parseJsonFields<T>(doc: T, jsonFields: Array<string | keyof T>) 
       item[key] = JSON.parse(item[key] as string)
     }
   }
+
+  for (const key in item) {
+    if (item[key] === 'NULL') {
+      item[key as keyof T] = undefined as unknown as T[keyof T]
+    }
+  }
   return item
 }
