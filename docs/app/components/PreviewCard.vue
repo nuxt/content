b68447b2f455f74418d6eaac023c556f0119fff6
Farnabaz <farnabaz@gmail.com>
2024-10-17 10:54:32 +0200

    docs: update structure

---
+++ docs/app/components/PreviewCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  prose: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+
+<template>
+  <div
+    class="p-4 border rounded-b-md border-[--ui-color-neutral-200] dark:border-[--ui-color-neutral-700] bg-[--ui-color-neutral-50] dark:bg-[--ui-color-neutral-800] dark:text-white"
+    :class="prose ? 'prose' : 'not-prose'"
+  >
+    <slot />
+  </div>
+</template>
