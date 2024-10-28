0b6c9502ef5a4056583e097879cd0c2526f75aff
Farnabaz <farnabaz@gmail.com>
2024-10-28 16:17:20 +0100

    feat: vercel preset

--- src/presets/index.ts
+++ src/presets/index.ts
@@ -1,10 +1,17 @@
 import type { Nuxt } from '@nuxt/schema'
 import cloudflare from './cloudflare-pages'
+import vercel from './vercel'
 import node from './node'
 
 export function findPreset(nuxt: Nuxt) {
-  if (nuxt.options.nitro.preset === 'cloudflare-pages') {
+  const preset = nuxt.options.nitro.preset
+
+  if (preset === 'cloudflare-pages') {
     return cloudflare
+  }
+
+  if (preset === 'vercel' || process.env.VERCEL === '1') {
+    return vercel
   }
 
   return node
---
+++ src/presets/vercel.ts
@@ -0,0 +1,17 @@
+import { definePreset } from '../utils/preset'
+import { logger } from '../utils/dev'
+import nodePreset from './node'
+
+export default definePreset({
+  async setupNitro(nitroConfig, options) {
+    if (nitroConfig.runtimeConfig?.content?.database?.type === 'sqlite') {
+      logger.warn('Deploying sqlite database to Vercel is not possible, switching to Postgres database with `POSTGRES_URL`.')
+      nitroConfig.runtimeConfig!.content!.database = {
+        type: 'postgres',
+        url: 'process.env.POSTGRES_URL',
+      }
+    }
+
+    await nodePreset.setupNitro(nitroConfig, options)
+  },
+})
