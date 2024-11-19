d6e13deb6521422fca1a322e3472c5a9726eaebc
Benjamin Canac <canacb1@gmail.com>
2024-11-19 21:33:23 +0100

    chore(deps): update `@nuxt/ui-pro`

--- docs/app/app.vue
+++ docs/app/app.vue
@@ -63,7 +63,7 @@ provide('navigation', navigation)
 @source "../content/**/*";
 
 @theme {
-  --font-family-sans: 'Public Sans', sans-serif;
+  --font-sans: 'Public Sans', sans-serif;
 
   --color-green-50: #EFFDF5;
   --color-green-100: #D9FBE8;
@@ -79,7 +79,7 @@ provide('navigation', navigation)
 }
 
 :root {
-  --ui-container-width: 90rem;
+  --ui-container: 90rem;
 }
 
 .dark {
