2f7723855e41a1d1867785c855f48cd981bab76e
Baptiste Leproux <leproux.baptiste@gmail.com>
2024-11-22 12:04:00 +0100

    studio export

--- docs/nuxt.schema.ts
+++ docs/nuxt.schema.ts
@@ -1,4 +1,4 @@
-import { field, group } from '@nuxt/content'
+import { field, group } from '@nuxt/content/studio'
 
 export default defineNuxtSchema({
   appConfig: {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
 export { metaSchema, pageSchema } from './schema'
 export { defineCollection } from './collection'
-export { field, group } from './studio/data'
 export { z } from './zod'
