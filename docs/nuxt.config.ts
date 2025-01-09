5b49b167773fd5b71e841af3bda3e126e1c345da
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-01-09 14:02:21 +0100

    rmove gitinfo

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -42,10 +42,10 @@ export default defineNuxtConfig({
     studio: {
       enabled: true,
       dev: true,
-      gitInfo: {
-        owner: 'larbish',
-        name: 'starter-larb',
-      },
+      // gitInfo: {
+      //   owner: 'larbish',
+      //   name: 'starter-larb',
+      // },
     },
   },
 
