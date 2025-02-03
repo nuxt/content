083a47a1461735a3e0196d04e4df9f828cfc36af
Farnabaz <farnabaz@gmail.com>
2025-02-03 15:50:20 +0100

    fix(nuxthub): split queries into 1MB chunks

--- src/presets/nuxthub.ts
+++ src/presets/nuxthub.ts
@@ -15,9 +15,28 @@ export default definePreset({
 
     if (nitroConfig.dev === false) {
       // Write SQL dump to database queries when not in dev mode
-      const sql = Object.values(options.manifest.dump).map(value => value.join('\n')).join('\n')
       await mkdir(resolve(nitroConfig.rootDir, '.data/hub/database/queries'), { recursive: true })
-      await writeFile(resolve(nitroConfig.rootDir, '.data/hub/database/queries/content-database.sql'), sql)
+      let i = 1
+      let dump = ''
+      const dumpFiles: Array<{ file: string, content: string }> = []
+      Object.values(options.manifest.dump).forEach((value) => {
+        value.forEach((line) => {
+          if ((dump.length + line.length) < 1000000) {
+            dump += line + '\n'
+          }
+          else {
+            dumpFiles.push({ file: `content-database-${String(i).padStart(3, '0')}.sql`, content: dump.trim() })
+            dump = line
+            i += 1
+          }
+        })
+      })
+      if (dump.length > 0) {
+        dumpFiles.push({ file: `content-database-${String(i).padStart(3, '0')}.sql`, content: dump.trim() })
+      }
+      for (const dumpFile of dumpFiles) {
+        await writeFile(resolve(nitroConfig.rootDir, '.data/hub/database/queries', dumpFile.file), dumpFile.content)
+      }
       // Disable integrity check in production for performance
       nitroConfig.runtimeConfig.content.integrityCheck = false
     }
