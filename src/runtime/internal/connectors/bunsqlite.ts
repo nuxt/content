7477d3758314a6d5cd418b083ab44aaa58d775d9
Artem Melnyk <melkam464@gmail.com>
2025-02-21 13:19:04 +0100

    fix: bunsqlite polyfills not being loaded

--- src/runtime/internal/connectors/bunsqlite.ts
+++ src/runtime/internal/connectors/bunsqlite.ts
@@ -1,43 +1,57 @@
 import * as zlib from 'node:zlib'
+import bunSqliteConnector from 'db0/connectors/bun-sqlite'
 
-/**
- * CompressionStream and DecompressionStream polyfill for Bun
- */
-if (!globalThis.CompressionStream) {
+const connector: typeof bunSqliteConnector = (opts) => {
+  /**
+   * CompressionStream and DecompressionStream polyfill for Bun
+   */
+  if (!globalThis.CompressionStream) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const make = (ctx: unknown, handle: any) => Object.assign(ctx, {
-    writable: new WritableStream({
-      write: (chunk: ArrayBufferView) => handle.write(chunk),
-      close: () => handle.end(),
-    }),
-    readable: new ReadableStream({
-      type: 'bytes',
-      start(ctrl) {
-        handle.on('data', (chunk: ArrayBufferView) => ctrl.enqueue(chunk))
-        handle.once('end', () => ctrl.close())
-      },
-    }),
-  })
+    const make = (ctx: unknown, handle: any) =>
+      Object.assign(ctx, {
+        writable: new WritableStream({
+          write: (chunk: ArrayBufferView) => handle.write(chunk),
+          close: () => handle.end(),
+        }),
+        readable: new ReadableStream({
+          type: 'bytes',
+          start(ctrl) {
+            handle.on('data', (chunk: ArrayBufferView) => ctrl.enqueue(chunk))
+            handle.once('end', () => ctrl.close())
+          },
+        }),
+      })
 
-  // eslint-disable-next-line @typescript-eslint/no-extraneous-class
-  globalThis.CompressionStream = class CompressionStream {
-    constructor(format: string) {
-      make(this, format === 'deflate'
-        ? zlib.createDeflate()
-        : format === 'gzip' ? zlib.createGzip() : zlib.createDeflateRaw())
-    }
-  } as unknown as typeof CompressionStream
+    // eslint-disable-next-line @typescript-eslint/no-extraneous-class
+    globalThis.CompressionStream = class CompressionStream {
+      constructor(format: string) {
+        make(
+          this,
+          format === 'deflate'
+            ? zlib.createDeflate()
+            : format === 'gzip'
+              ? zlib.createGzip()
+              : zlib.createDeflateRaw(),
+        )
+      }
+    } as unknown as typeof CompressionStream
 
-  // eslint-disable-next-line @typescript-eslint/no-extraneous-class
-  globalThis.DecompressionStream = class DecompressionStream {
-    constructor(format: string) {
-      make(this, format === 'deflate'
-        ? zlib.createInflate()
-        : format === 'gzip'
-          ? zlib.createGunzip()
-          : zlib.createInflateRaw())
-    }
-  } as unknown as typeof DecompressionStream
+    // eslint-disable-next-line @typescript-eslint/no-extraneous-class
+    globalThis.DecompressionStream = class DecompressionStream {
+      constructor(format: string) {
+        make(
+          this,
+          format === 'deflate'
+            ? zlib.createInflate()
+            : format === 'gzip'
+              ? zlib.createGunzip()
+              : zlib.createInflateRaw(),
+        )
+      }
+    } as unknown as typeof DecompressionStream
+  }
+
+  return bunSqliteConnector(opts)
 }
 
-export { default as default } from 'db0/connectors/bun-sqlite'
+export default connector
