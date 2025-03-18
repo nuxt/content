f0c501b052e87364f52e5b8794fe6214f3de37d4
Farnabaz <farnabaz@gmail.com>
2025-03-18 15:30:54 +0100

    fix pick utils

--- src/runtime/internal/utils.ts
+++ src/runtime/internal/utils.ts
@@ -4,11 +4,8 @@
 export function pick(keys?: string[]) {
   return (obj: Record<string, unknown>) => {
     obj = obj || {}
-    if (keys && keys.length) {
-      return keys
-        .filter(key => typeof obj[key] !== 'undefined')
-        .reduce((newObj, key) => Object.assign(newObj, { [key]: obj[key] }), {})
-    }
-    return obj
+    return (keys || [])
+      .filter(key => typeof obj[key] !== 'undefined')
+      .reduce((newObj, key) => Object.assign(newObj, { [key]: obj[key] }), {})
   }
 }
