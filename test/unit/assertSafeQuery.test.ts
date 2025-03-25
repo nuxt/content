d7b0a81da0f261655cc9b9fbd42acdc1417290c9
Gerard Wilkinson <gerard@ignite.systems>
2025-03-25 09:54:44 +0000

    fix tests

--- test/unit/assertSafeQuery.test.ts
+++ test/unit/assertSafeQuery.test.ts
@@ -5,7 +5,7 @@ import { collectionQueryBuilder } from '../../src/runtime/internal/query'
 // Mock tables from manifest
 vi.mock('#content/manifest', () => ({
   tables: {
-    test: '_content_test',
+    test: '_content_test_xxxx',
   },
 }))
 const mockFetch = vi.fn().mockResolvedValue(Promise.resolve([{}]))
@@ -21,29 +21,29 @@ describe('decompressSQLDump', () => {
     'SELECT * FROM sqlite_master': false,
     'INSERT INTO _test VALUES (\'abc\')': false,
     'CREATE TABLE _test (id TEXT PRIMARY KEY)': false,
-    'select * from _content_test ORDER BY id DESC': false,
-    ' SELECT * FROM _content_test ORDER BY id DESC': false,
-    'SELECT * FROM _content_test ORDER BY id DESC ': false,
-    'SELECT * FROM _content_test ORDER BY id DESC': true,
-    'SELECT * FROM _content_test ORDER BY id ASC,stem DESC': false,
-    'SELECT * FROM _content_test ORDER BY id ASC, stem DESC': true,
-    'SELECT * FROM _content_test ORDER BY id ASC, publishedAt DESC': true,
-    'SELECT "PublishedAt" FROM _content_test ORDER BY id ASC, PublishedAt DESC': true,
-    'SELECT * FROM _content_test ORDER BY id DESC -- comment is not allowed': false,
-    'SELECT * FROM _content_test ORDER BY id DESC; SELECT * FROM _content_test ORDER BY id DESC': false,
-    'SELECT * FROM _content_test ORDER BY id DESC LIMIT 10': true,
-    'SELECT * FROM _content_test ORDER BY id DESC LIMIT 10 OFFSET 10': true,
+    'select * from _content_test_xxxx ORDER BY id DESC': false,
+    ' SELECT * FROM _content_test_xxxx ORDER BY id DESC': false,
+    'SELECT * FROM _content_test_xxxx ORDER BY id DESC ': false,
+    'SELECT * FROM _content_test_xxxx ORDER BY id DESC': true,
+    'SELECT * FROM _content_test_xxxx ORDER BY id ASC,stem DESC': false,
+    'SELECT * FROM _content_test_xxxx ORDER BY id ASC, stem DESC': true,
+    'SELECT * FROM _content_test_xxxx ORDER BY id ASC, publishedAt DESC': true,
+    'SELECT "PublishedAt" FROM _content_test_xxxx ORDER BY id ASC, PublishedAt DESC': true,
+    'SELECT * FROM _content_test_xxxx ORDER BY id DESC -- comment is not allowed': false,
+    'SELECT * FROM _content_test_xxxx ORDER BY id DESC; SELECT * FROM _content_test_xxxx ORDER BY id DESC': false,
+    'SELECT * FROM _content_test_xxxx ORDER BY id DESC LIMIT 10': true,
+    'SELECT * FROM _content_test_xxxx ORDER BY id DESC LIMIT 10 OFFSET 10': true,
     // Where clause should follow query builder syntax
-    'SELECT * FROM _content_test WHERE id = 1 ORDER BY id DESC LIMIT 10 OFFSET 10': false,
-    'SELECT * FROM _content_test WHERE (id = 1) ORDER BY id DESC LIMIT 10 OFFSET 10': true,
-    'SELECT * FROM _content_test WHERE (id = \'");\'); select * from ((SELECT * FROM sqlite_master where 1 <> "") as t) ORDER BY type DESC': false,
-    'SELECT "body" FROM _content_test ORDER BY body ASC': true,
+    'SELECT * FROM _content_test_xxxx WHERE id = 1 ORDER BY id DESC LIMIT 10 OFFSET 10': false,
+    'SELECT * FROM _content_test_xxxx WHERE (id = 1) ORDER BY id DESC LIMIT 10 OFFSET 10': true,
+    'SELECT * FROM _content_test_xxxx WHERE (id = \'");\'); select * from ((SELECT * FROM sqlite_master where 1 <> "") as t) ORDER BY type DESC': false,
+    'SELECT "body" FROM _content_test_xxxx ORDER BY body ASC': true,
     // Advanced
-    'SELECT COUNT(*) UNION SELECT name /**/FROM sqlite_master-- FROM _content_test WHERE (1=1) ORDER BY id ASC': false,
-    'SELECT * FROM _content_test WHERE (id /*\'*/IN (SELECT id FROM _content_test) /*\'*/) ORDER BY id ASC': false,
-    'SELECT * FROM _content_test WHERE (1=\' \\\' OR id IN (SELECT id FROM _content_docs) OR 1!=\'\') ORDER BY id ASC': false,
-    'SELECT "id", "id" FROM _content_docs WHERE (1=\' \\\') UNION SELECT tbl_name,tbl_name FROM sqlite_master-- \') ORDER BY id ASC': false,
-    'SELECT "id" FROM _content_test WHERE (x=$\'$ OR x IN (SELECT BLAH) OR x=$\'$) ORDER BY id ASC': false,
+    'SELECT COUNT(*) UNION SELECT name /**/FROM sqlite_master-- FROM _content_test_xxxx WHERE (1=1) ORDER BY id ASC': false,
+    'SELECT * FROM _content_test_xxxx WHERE (id /*\'*/IN (SELECT id FROM _content_test_xxxx) /*\'*/) ORDER BY id ASC': false,
+    'SELECT * FROM _content_test_xxxx WHERE (1=\' \\\' OR id IN (SELECT id FROM _content_docs_xxxx) OR 1!=\'\') ORDER BY id ASC': false,
+    'SELECT "id", "id" FROM _content_docs_xxxx WHERE (1=\' \\\') UNION SELECT tbl_name,tbl_name FROM sqlite_master-- \') ORDER BY id ASC': false,
+    'SELECT "id" FROM _content_test_xxxx WHERE (x=$\'$ OR x IN (SELECT BLAH) OR x=$\'$) ORDER BY id ASC': false,
   }
 
   Object.entries(queries).forEach(([query, isValid]) => {
