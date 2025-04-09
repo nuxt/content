754e4803da230368e9015d264928e0d2aee3b7a3
Farnabaz <farnabaz@gmail.com>
2025-04-09 18:33:31 +0200

    fix: ignore types

--- docs/server/plugins/llms.ts
+++ docs/server/plugins/llms.ts
@@ -1,6 +1,7 @@
 import { visit, decompressTree } from '@nuxt/content/runtime'
 
 export default defineNitroPlugin((nitroApp) => {
+  // @ts-expect-error -- TODO: fix types
   nitroApp.hooks.hook('content:llms:generate:document', async (doc) => {
     visit(doc.body, node => node[0] === 'note', (node) => {
       const _tree = decompressTree({ type: 'minimal', value: [node] })
