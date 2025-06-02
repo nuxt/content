d4ecaa7cb7d587515f1da725ef131c9afe24f8a9
Oliver-Tobias Ripka <otr@bockcay.de>
2025-06-02 09:02:55 +0200

    fix: forward SSR cookies in internal API requests (#3385)

--- src/runtime/internal/api.ts
+++ src/runtime/internal/api.ts
@@ -5,7 +5,10 @@ export async function fetchDatabase(event: H3Event | undefined, collection: stri
   return await $fetch(`/__nuxt_content/${collection}/sql_dump.txt`, {
     context: event ? { cloudflare: event.context.cloudflare } : {},
     responseType: 'text',
-    headers: { 'content-type': 'text/plain' },
+    headers: {
+      'content-type': 'text/plain',
+      ...(event?.node?.req?.headers?.cookie ? { cookie: event.node.req.headers.cookie } : {}),
+    },
     query: { v: checksums[String(collection)], t: import.meta.dev ? Date.now() : undefined },
   })
 }
@@ -13,7 +16,10 @@ export async function fetchDatabase(event: H3Event | undefined, collection: stri
 export async function fetchQuery<Item>(event: H3Event | undefined, collection: string, sql: string): Promise<Item[]> {
   return await $fetch(`/__nuxt_content/${collection}/query`, {
     context: event ? { cloudflare: event.context.cloudflare } : {},
-    headers: { 'content-type': 'application/json' },
+    headers: {
+      'content-type': 'application/json',
+      ...(event?.node?.req?.headers?.cookie ? { cookie: event.node.req.headers.cookie } : {}),
+    },
     query: { v: checksums[String(collection)], t: import.meta.dev ? Date.now() : undefined },
     method: 'POST',
     body: {
