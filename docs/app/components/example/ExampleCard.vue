4e0a8881101b95c65b80a365b94bb185b98f1ac4
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-06-19 22:08:53 +0200

    remove docs

--- docs/app/components/example/ExampleCard.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="p-2 border bg-white dark:bg-black dark:border-gray-700 rounded">
-    <slot />
-  </div>
-</template>
--- docs/app/components/example/ExampleTitle.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <h1 class="text-4xl">
-    <slot mdc-unwrap="p" />
-  </h1>
-</template>
