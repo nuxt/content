9b4b4f2e910b1bbdf979235b15ec8962eeaf14fa
Farnabaz <farnabaz@gmail.com>
2025-12-15 17:08:59 +0100

    fix(database): sqlite memory driver syntax (#3635)

--- src/runtime/internal/database.server.ts
+++ src/runtime/internal/database.server.ts
@@ -212,7 +212,7 @@ function refineDatabaseConfig(config: RuntimeConfig['content']['database']) {
   if (config.type === 'sqlite') {
     const _config = { ...config } as SqliteConnectorOptions
     if (config.filename === ':memory:') {
-      return { name: 'memory' }
+      return { name: ':memory:' }
     }
 
     if ('filename' in config) {
