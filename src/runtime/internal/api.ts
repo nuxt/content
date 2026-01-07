046aae5af49708235f6860bf390f2aab2c651797
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-01-07 10:12:32 +0100

    feat(api): use request fetch

--- src/runtime/internal/api.ts
+++ src/runtime/internal/api.ts
@@ -1,24 +1,26 @@
-import type { H3Event } from 'h3'
+import { getRequestHeaders, type H3Event } from 'h3'
 import { checksums } from '#content/manifest'
 
 export async function fetchDatabase(event: H3Event | undefined, collection: string): Promise<string> {
-  return await $fetch(`/__nuxt_content/${collection}/sql_dump.txt`, {
-    context: event ? { cloudflare: event.context.cloudflare } : {},
+  const headers = event ? getRequestHeaders(event) : {}
+  const fetch = event?.$fetch || $fetch
+  return await fetch(`/__nuxt_content/${collection}/sql_dump.txt`, {
     responseType: 'text',
     headers: {
+      ...headers,
       'content-type': 'text/plain',
-      ...(event?.node?.req?.headers?.cookie ? { cookie: event.node.req.headers.cookie } : {}),
     },
     query: { v: checksums[String(collection)], t: import.meta.dev ? Date.now() : undefined },
   })
 }
 
 export async function fetchQuery<Item>(event: H3Event | undefined, collection: string, sql: string): Promise<Item[]> {
-  return await $fetch(`/__nuxt_content/${collection}/query`, {
-    context: event ? { cloudflare: event.context.cloudflare } : {},
+  const headers = event ? getRequestHeaders(event) : {}
+  const fetch = event?.$fetch || $fetch
+  return await fetch(`/__nuxt_content/${collection}/query`, {
     headers: {
+      ...headers,
       'content-type': 'application/json',
-      ...(event?.node?.req?.headers?.cookie ? { cookie: event.node.req.headers.cookie } : {}),
     },
     query: { v: checksums[String(collection)], t: import.meta.dev ? Date.now() : undefined },
     method: 'POST',
