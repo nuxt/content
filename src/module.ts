6e99c7d194ea979ea94d2ffeb45a6c80cc3f6ef9
Farnabaz <farnabaz@gmail.com>
2026-01-15 17:54:59 +0100

    chore: upgrade deps

--- src/module.ts
+++ src/module.ts
@@ -445,7 +445,7 @@ async function processCollectionItems(nuxt: Nuxt, collections: ResolvedCollectio
     ...new Set(usedComponents),
   ]
     .map(tag => getMappedTag(tag, options?.renderer?.alias))
-    .filter(tag => !htmlTags.includes(kebabCase(tag)))
+    .filter(tag => !htmlTags.has(kebabCase(tag)))
     .map(tag => pascalCase(tag))
 
   const endTime = performance.now()
