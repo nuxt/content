ae179d968c65b99fb9c88244adcdc3c01b4bb1dc
Farnabaz <farnabaz@gmail.com>
2026-01-15 17:30:26 +0100

    chore: upgrade deps

--- src/runtime/nitro.ts
+++ src/runtime/nitro.ts
@@ -1,4 +1,7 @@
 import * as server from './server'
+import type { Collections, CollectionQueryBuilder } from '@nuxt/content'
+import type { H3Event } from 'h3'
+
 /**
  * `@nuxt/content/nitro` import is deprecated and will be removed in the next major version.
  * Use `@nuxt/content/server` instead.
@@ -7,7 +10,9 @@ import * as server from './server'
 /**
  * @deprecated Import from `@nuxt/content/server` instead
  */
-export const queryCollection = server.queryCollection
+export const queryCollection = <T extends keyof Collections>(event: H3Event, collection: T): CollectionQueryBuilder<Collections[T]> => {
+  return server.queryCollection(event, collection)
+}
 
 /**
  * @deprecated Import from `@nuxt/content/server` instead
