8843e4cbef8f0a4633b3842c0dedc6a268a374a8
Baptiste Leproux <leproux.baptiste@gmail.com>
2026-02-25 11:58:17 +0100

    fix(llms): remove external package import

--- src/features/llms/runtime/server/utils.ts
+++ src/features/llms/runtime/server/utils.ts
@@ -1,4 +1,4 @@
-import type { MDCElement } from '@nuxtjs/mdc'
+import type { MDCElement, MDCRoot } from '@nuxtjs/mdc'
 import { withBase } from 'ufo'
 import { pascalCase } from 'scule'
 import type { LLMsSection } from 'nuxt-llms'
@@ -6,6 +6,8 @@ import { toHast } from 'minimark/hast'
 import type { MinimarkTree } from 'minimark'
 import type { PageCollectionItemBase, SQLOperator } from '@nuxt/content'
 import manifest from '#content/manifest'
+import { visit } from 'unist-util-visit'
+import { stringifyMarkdown } from '@nuxtjs/mdc/runtime'
 
 export interface ContentLLMSCollectionSection extends LLMsSection {
   __nuxt_content_auto_generate?: boolean
@@ -19,17 +21,12 @@ export interface ContentLLMSCollectionSection extends LLMsSection {
 
 const linkProps = ['href', 'src', 'to']
 
-const importExternalPackage = async (name: string) => await import(name)
-
 export async function createDocumentGenerator() {
-  const visit = await importExternalPackage('unist-util-visit').then(res => res.visit)
-  const stringifyMarkdown = await importExternalPackage('@nuxtjs/mdc/runtime').then(res => res.stringifyMarkdown)
-
   return generateDocument
 
   async function generateDocument(doc: PageCollectionItemBase, options: { domain: string }) {
     const hastTree = refineDocumentBody(doc.body as unknown as MinimarkTree, options)
-    let markdown = await stringifyMarkdown(hastTree, {})
+    let markdown = await stringifyMarkdown(hastTree as unknown as MDCRoot, {})
 
     if (!markdown?.trim().startsWith('# ')) {
       const title = doc.title || doc.seo?.title || ''
@@ -43,10 +40,14 @@ export async function createDocumentGenerator() {
   function refineDocumentBody(body: MinimarkTree, options: { domain: string }) {
     const hastTree = toHast(body)
 
-    visit(hastTree, (node: MDCElement) => !!node.props?.to || !!node.props?.href || !!node.props?.src, (node: MDCElement) => {
+    visit(hastTree, (node) => {
+      const el = node as unknown as MDCElement
+      if (!el.props?.to && !el.props?.href && !el.props?.src) {
+        return
+      }
       for (const prop of linkProps) {
-        if (node.props?.[prop]) {
-          node.props[prop] = withBase(node.props[prop], options.domain)
+        if (el.props?.[prop]) {
+          el.props[prop] = withBase(el.props[prop], options.domain)
         }
       }
     })
